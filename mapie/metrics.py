bbe2b9b87d44779ab9b4b2553b2b43d02bb0d8b8
Vianney Taquet <vtaquet@quantmetry.com>
2021-04-13 14:25:53 +0200

    Add coverage metrics and test.

---
+++ mapie/metrics.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def coverage(y_true: np.ndarray, y_preds: np.ndarray) -> float:
+    """
+    Effective coverage obtained by the prediction intervals.
+
+    The effective coverage is obtained by estimating the fraction
+    of true labels that lie within the prediction intervals.
+
+    Parameters
+    ----------
+    y_true : np.ndarray of shape (n_samples,)
+        True labels.
+    y_preds : np.ndarray of shape (n_samples, 3)
+        Predictions as returned by `MapieRegressor.predict()`.
+
+    Returns
+    -------
+    float
+        Effective coverage obtained by the prediction intervals.
+
+    Examples
+    --------
+    >>> from mapie.metrics import coverage
+    >>> y_true = np.array([5, 7.5, 9.5, 10.5, 12.5])
+    >>> y_preds = np.array([
+        [5, 4, 6],
+        [7.5, 6., 9.],
+        [9.5, 9, 10.],
+        [10.5, 8.5, 12.5],
+        [11.5, 10.5, 12.]
+    ])
+    >>> print(coverage(y_true, y_preds))
+    0.8
+    """
+    if not isinstance(y_true, np.ndarray):
+        raise ValueError("y_true is not an np.ndarray.")
+    if not isinstance(y_preds, np.ndarray):
+        raise ValueError("y_preds is not an np.ndarray.")
+    if y_true.shape[0] != y_preds.shape[0]:
+        raise ValueError("y_true and y_preds have different lengths.")
+    if y_preds.shape[1] != 3:
+        raise ValueError("y_preds.shape[1] is not equal to 3.")
+    return (
+        (y_preds[:, 1] <= y_true) & (y_preds[:, 2] >= y_true)
+    ).mean()
---
+++ mapie/tests/test_metrics.py
@@ -0,0 +1,46 @@
+"""
+Testing for metrics module.
+"""
+import pytest
+import numpy as np
+from mapie.metrics import coverage
+
+
+X_toy = np.array([0, 1, 2, 3, 4]).reshape(-1, 1)
+y_toy = np.array([5, 7.5, 9.5, 10.5, 12.5])
+y_toy_preds = np.array([
+    [5, 4, 6],
+    [7.5, 6., 9.],
+    [9.5, 9, 10.],
+    [10.5, 8.5, 12.5],
+    [11.5, 10.5, 12.]
+])
+
+
+def test_ytrue_type() -> None:
+    "Test that y_true is a numpy array."
+    with pytest.raises(ValueError, match=r".*not an np.ndarray*"):
+        coverage(list(y_toy), y_toy_preds)
+
+
+def test_ypreds_type() -> None:
+    "Test that y_preds is a numpy array."
+    with pytest.raises(ValueError, match=r".*not an np.ndarray*"):
+        coverage(y_toy, list(y_toy_preds))
+
+
+def test_same_length() -> None:
+    "Test when y_true and y_preds have different lengths."
+    with pytest.raises(ValueError, match=r".*different lengths*"):
+        coverage(y_toy, y_toy_preds[:-1, :])
+
+
+def test_ypreds_shape() -> None:
+    "Test that y_preds.shape[1] is equal to 3."
+    with pytest.raises(ValueError, match=r".*not equal to 3*"):
+        coverage(y_toy, y_toy_preds[:, :2])
+
+
+def test_toydata() -> None:
+    "Test coverage for toy data"
+    assert (coverage(y_toy, y_toy_preds) == 0.8)
