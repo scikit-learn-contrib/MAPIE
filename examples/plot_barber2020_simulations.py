1b6818c16184aa13fcff63694fa7bd1814a21c8c
gmartinonQM <gmartinon@quantmetry.com>
2021-04-16 08:27:50 +0200

    naive up to dim 180

--- examples/plot_barber2020_simulations.py
+++ examples/plot_barber2020_simulations.py
@@ -174,6 +174,6 @@ methods = [
 ]
 alpha = 0.1
 ntrial = 1
-dimensions = np.arange(20, 220, 20)
+dimensions = np.arange(20, 200, 20)
 results = PIs_vs_dimensions(methods, alpha, ntrial, dimensions)
 plot_simulation_results(results, title="Coverages and interval widths")
