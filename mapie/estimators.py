beb66c9d550ac5b67536f5a8521863eb3a1411b9
gmartinonQM <gmartinon@quantmetry.com>
2021-04-22 18:16:41 +0200

    update syntax

--- mapie/estimators.py
+++ mapie/estimators.py
@@ -103,7 +103,7 @@ class MapieRegressor(BaseEstimator, RegressorMixin):  # type: ignore
     Examples
     --------
     >>> import numpy as np
-    >>> from mapie.mapieregressor import MapieRegressor
+    >>> from mapie.estimators import MapieRegressor
     >>> from sklearn.linear_model import LinearRegression
     >>> X_toy = np.array([0, 1, 2, 3, 4, 5]).reshape(-1, 1)
     >>> y_toy = np.array([5, 7.5, 9.5, 10.5, 12.5, 15])
