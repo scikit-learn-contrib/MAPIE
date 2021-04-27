e158c560ab887464925e0af1b68fe3a40631182d
gmartinonQM <gmartinon@quantmetry.com>
2021-04-27 17:25:10 +0200

    fix doc/conf.py typo

--- doc/conf.py
+++ doc/conf.py
@@ -275,9 +275,15 @@ man_pages = [
 # (source start file, target name, title, author,
 # dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'mapie', u'MAPIE Documentation',
-   u'Quantmetry', 'MAPIE', 'One line description of project.',
-   'Miscellaneous'),
+    (
+        'index',
+        'mapie',
+        u'MAPIE Documentation',
+        u'Quantmetry',
+        'MAPIE',
+        'One line description of project.',
+        'Miscellaneous'
+    ),
 ]
 
 # Documents to append as an appendix to all manuals.
