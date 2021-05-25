36c8c7dda497b45380a06a45a3c00dbe71d94661
Vianney Taquet <vtaquet@quantmetry.com>
2021-05-25 15:52:44 +0200

    Fix CI

--- mapie/estimators.py
+++ mapie/estimators.py
@@ -403,9 +403,7 @@ class MapieRegressor(BaseEstimator, RegressorMixin):  # type: ignore
             - [3]: Validation data indices, of shapes (n_samples_val,)
         """
         X_train, y_train, X_val = X[train_index], y[train_index], X[val_index]
-        sample_weight_train = sample_weight.copy()
-        if sample_weight is not None:
-            sample_weight_train = sample_weight[train_index]
+        sample_weight_train = sample_weight[train_index] if sample_weight is not None else None
         estimator = self._fit_estimator(estimator, X_train, y_train, supports_sw, sample_weight_train)
         y_pred = estimator.predict(X_val)
         val_id = np.full_like(y_pred, k)
