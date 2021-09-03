bb29996c96c50285b0aa47eabc8bc003c5e6a7ac
AdirthaBorgohain <adirthaborgohain@gmail.com>
2021-09-03 17:22:25 +0530

    Bump version: 0.2.3 → 0.2.4

--- doc/conf.py
+++ doc/conf.py
@@ -85,7 +85,7 @@ copyright = u"2021, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "0.2.3"
+version = "0.2.4"
 # The full version, including alpha/beta/rc tags.
 release = version
 
