d20903f0c6d8b669ed82089a9aadd294233e1f0e
Vincent Blot <vblot@quantmetry.com>
2022-03-11 17:17:55 +0100

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 
 DISTNAME = "MAPIE"
-VERSION = "0.3.1"
+VERSION = "0.3.2"
 DESCRIPTION = (
     "A scikit-learn-compatible module "
     "for estimating prediction intervals."
