59ec655d933066addf7a316c9d7f436b04935ec9
gmartinonQM <gmartinon@quantmetry.com>
2022-03-22 14:03:33 +0100

    FIX missing unit test

--- mapie/tests/test_utils.py
+++ mapie/tests/test_utils.py
@@ -89,7 +89,7 @@ def test_fit_estimator_sample_weight() -> None:
         np.testing.assert_almost_equal(y_pred_1, y_pred_2)
 
 
-@pytest.mark.parametrize("alpha", [-1, 0, 1, 2, 2.5, "a", ["a", "b"]])
+@pytest.mark.parametrize("alpha", [-1, 0, 1, 2, 2.5, [0.4, 0.6], "a", ["a", "b"]])
 def test_invalid_alpha(alpha: Any) -> None:
     """Test that invalid alphas raise errors."""
     with pytest.raises(ValueError, match=r".*Invalid alpha.*"):
@@ -100,7 +100,6 @@ def test_invalid_alpha(alpha: Any) -> None:
     "alpha",
     [
         0.95,
-        np.linspace(0.05, 0.95, 5),
         [0.05, 0.95],
         (0.05, 0.95),
         np.array([0.05, 0.95]),
