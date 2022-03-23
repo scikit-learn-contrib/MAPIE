7d81794638e4da908f48167930529eeb5dc4fcda
gmartinonQM <gmartinon@quantmetry.com>
2022-03-23 10:12:54 +0100

    ENH make explicit numpy dependency

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ gmartinon@quantmetry.com
 """
 PYTHON_REQUIRES = ">=3.7"
 PACKAGES = find_packages()
-INSTALL_REQUIRES = ["scikit-learn"]
+INSTALL_REQUIRES = ["scikit-learn", "numpy>=1.22"]
 EXTRAS_REQUIRE = {
     "tests": [
         "flake8",
