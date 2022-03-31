700ddfafd41bc9c8dd2b798dad5b804b59421551
gmartinonQM <gmartinon@quantmetry.com>
2022-03-31 12:01:31 +0200

    ENH simplify _typing.py

--- mapie/_typing.py
+++ mapie/_typing.py
@@ -1,11 +1,3 @@
-import numpy as np
-from typing import Union, List
-
-
-try:
-    from numpy.typing import ArrayLike, NDArray
-except (AttributeError, ModuleNotFoundError, ImportError):
-    ArrayLike = Union[np.ndarray, List[List[float]]]  # type: ignore
-    NDArray = np.ndarray  # type: ignore
+from numpy.typing import ArrayLike, NDArray
 
 __all__ = ["ArrayLike", "NDArray"]
