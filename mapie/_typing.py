30780568f98630bd6a97bf58394bd4e2701e95ac
gmartinonQM <gmartinon@quantmetry.com>
2022-03-31 10:30:32 +0200

    ENH reintroduct try/except in _typing.py

--- mapie/_typing.py
+++ mapie/_typing.py
@@ -1,3 +1,11 @@
-from numpy.typing import ArrayLike, NDArray
+import numpy as np
+from typing import Union, List
+
+
+try:
+    from numpy.typing import ArrayLike, NDArray
+except (AttributeError, ModuleNotFoundError):
+    ArrayLike = Union[np.ndarray, List[List[float]]]
+    NDArray = np.ndarray
 
 __all__ = ["ArrayLike", "NDArray"]
