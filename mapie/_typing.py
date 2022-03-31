42837ed5b852e85fc474dacc82dcbbf774b164df
gmartinonQM <gmartinon@quantmetry.com>
2022-03-31 11:11:47 +0200

    ENH ignore useless type declarations

--- mapie/_typing.py
+++ mapie/_typing.py
@@ -5,7 +5,7 @@ from typing import Union, List
 try:
     from numpy.typing import ArrayLike, NDArray
 except (AttributeError, ModuleNotFoundError):
-    ArrayLike = Union[np.ndarray, List[List[float]]]
-    NDArray = np.ndarray
+    ArrayLike = Union[np.ndarray, List[List[float]]]  # type: ignore
+    NDArray = np.ndarray  # type: ignore
 
 __all__ = ["ArrayLike", "NDArray"]
