ec6072067fee8b7c822d1e152073ed7d0e76b829
Arnaud CAPITAINE <arnaud.gc.capitaine@gmail.com>
2022-06-17 13:24:26 +0200

    refactor: clean the tutorial comparing prediction intervals with different conformity scores

--- examples/regression/1-quickstart/plot_compare_conformity_scores.py
+++ examples/regression/1-quickstart/plot_compare_conformity_scores.py
@@ -10,8 +10,8 @@ We use here the OpenML house_prices dataset:
 https://www.openml.org/search?type=data&sort=runs&id=42165&status=active.
 
 The data is modelled by a Random Forest model
-:class:`sklearn.ensemble.RandomForestRegressor` with a fix parameter set.
-The confidence intervals are determined by means of the MAPIE regressor
+:class:`sklearn.ensemble.RandomForestRegressor` with a fixed parameter set.
+The prediction intervals are determined by means of the MAPIE regressor
 :class:`mapie.regression.MapieRegressor` considering two conformity scores:
 :class:`mapie.conformity_scores.AbsoluteConformityScore` which
 considers the absolute residuals as the conformity scores and
@@ -20,11 +20,11 @@ considers the residuals divided by the predicted means as conformity scores.
 We consider the standard CV+ resampling method.
 
 We would like to emphasize one main limitation with this example.
-With the default conformity score, the confidence intervals
+With the default conformity score, the prediction intervals
 are approximately equal over the range of house prices which may
 be inapporpriate when the price range is wide. The Gamma conformity score
-overcomes this issue by considering confidence intervals with width
-proportional to the predicted mean. For low prices, the Gamma confidence
+overcomes this issue by considering prediction intervals with width
+proportional to the predicted mean. For low prices, the Gamma prediction
 intervals are narrower than the default ones, conversely to high prices
 for which the conficence intervals are higher but visually more relevant.
 The empirical coverage is similar between the two conformity scores.
@@ -53,13 +53,13 @@ features = [
 alpha = 0.05
 rf_kwargs = {"n_estimators": 10, "random_state": 0}
 
-##############################################################################
+###############################################################################
 # 1. Load dataset with a target following approximativeley a Gamma distribution
-# --------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #
 # We start by loading a dataset with a target following approximately
 # a Gamma distribution. The GammaConformityScore is relevant in such cases.
-# Two sub dataset are extracted: the training and test ones.
+# Two sub datasets are extracted: the training and test ones.
 
 X, y = fetch_openml(name="house_prices", return_X_y=True)
 
@@ -67,37 +67,53 @@ X_train, X_test, y_train, y_test = train_test_split(
     X[features], y, test_size=0.2
 )
 
-##############################################################################
+###########################################
 # 2. Train model with two conformity scores
-# --------------------------------------------------------------------------
+# -----------------------------------------
 #
 # Two models are trained with two different conformity score:
-# - AbsoluteConformityScore (default conformity score) relevant for target
-#   positive as well as negative. The confidence interval widths are in this
-#   case approximately the same over the range of prediction.
-# - GammaConformityScore relevant for target following roughly a Gamma
-#   distribution. The confidence interval widths scale with the predicted
-#   value.
+#
+# - :class:mapie.conformity_scores.AbsoluteConformityScore (default conformity
+#   score) relevant for target positive as well as negative.
+#   The prediction interval widths are, in this case, approximately the same
+#   over the range of prediction.
+#
+# - :class:mapie.conformity_scores.GammaConformityScore relevant for target
+#   following roughly a Gamma distribution. The prediction interval widths
+#   scale with the predicted value.
 
-# Train model with AbsoluteConformityScore
+##############################################################################
+# First, train model with
+# :class:mapie.conformity_scores.AbsoluteConformityScore.
 mapie = MapieRegressor(RandomForestRegressor(**rf_kwargs))
 mapie.fit(X_train, y_train)
-y_pred_absconfscore, y_pis_absconfscore = mapie.predict(X_test, alpha=[alpha])
-yerr_absconfscore = np.concatenate(
-    [
-        np.expand_dims(y_pred_absconfscore, 0) - y_pis_absconfscore[:, 0, 0].T,
-        y_pis_absconfscore[:, 1, 0].T - np.expand_dims(y_pred_absconfscore, 0),
-    ],
-    axis=0,
-)
-conf_int_width_absconfscore = (
-    y_pis_absconfscore[:, 1, 0] - y_pis_absconfscore[:, 0, 0]
-)
+y_pred_absconfscore, y_pis_absconfscore = mapie.predict(X_test, alpha=alpha)
+
 coverage_absconfscore = regression_coverage_score(
     y_test, y_pis_absconfscore[:, 0, 0], y_pis_absconfscore[:, 1, 0]
 )
 
-# Train model with GammaConformityScore
+##############################################################################
+# Prepare the results for matplotlib. Get the prediction intervals and their
+# corresponding widths.
+def get_yerr(y_pred, y_pis):
+    return np.concatenate(
+        [
+            np.expand_dims(y_pred, 0) - y_pis[:, 0, 0].T,
+            y_pis[:, 1, 0].T - np.expand_dims(y_pred, 0),
+        ],
+        axis=0,
+    )
+
+
+yerr_absconfscore = get_yerr(y_pred_absconfscore, y_pis_absconfscore)
+pred_int_width_absconfscore = (
+    y_pis_absconfscore[:, 1, 0] - y_pis_absconfscore[:, 0, 0]
+)
+
+##############################################################################
+# Then, train the model with
+# :class:mapie.conformity_scores.GammaConformityScore.
 mapie = MapieRegressor(
     RandomForestRegressor(**rf_kwargs), conformity_score=GammaConformityScore()
 )
@@ -105,93 +121,68 @@ mapie.fit(X_train, y_train)
 y_pred_gammaconfscore, y_pis_gammaconfscore = mapie.predict(
     X_test, alpha=[alpha]
 )
-yerr_gammaconfscore = np.concatenate(
-    [
-        np.expand_dims(y_pred_gammaconfscore, 0)
-        - y_pis_gammaconfscore[:, 0, 0].T,
-        y_pis_gammaconfscore[:, 1, 0].T
-        - np.expand_dims(y_pred_gammaconfscore, 0),
-    ],
-    axis=0,
-)
-conf_int_width_gammaconfscore = (
-    y_pis_gammaconfscore[:, 1, 0] - y_pis_gammaconfscore[:, 0, 0]
-)
+
 coverage_gammaconfscore = regression_coverage_score(
     y_test, y_pis_gammaconfscore[:, 0, 0], y_pis_gammaconfscore[:, 1, 0]
 )
 
-##############################################################################
-# 3. Compare the confidence intervals
-# --------------------------------------------------------------------------
+yerr_gammaconfscore = get_yerr(y_pred_gammaconfscore, y_pis_gammaconfscore)
+pred_int_width_gammaconfscore = (
+    y_pis_gammaconfscore[:, 1, 0] - y_pis_gammaconfscore[:, 0, 0]
+)
+
+
+#####################################
+# 3. Compare the prediction intervals
+# -----------------------------------
 #
-# Once the models have been trained, we now compare the confidence intervals
+# Once the models have been trained, we now compare the prediction intervals
 # obtained from the two conformity scores. We can see that the
-# AbsoluteConformityScore generates confidence interval with almost the same
-# width for all the predicted values. Converly, the GammaConformityScore
-# yields confidence interval with width scaling with the predicted values.
+# :class:AbsoluteConformityScore generates prediction interval with almost the
+# same width for all the predicted values. Converly, the GammaConformityScore
+# yields prediction interval with width scaling with the predicted values.
 #
 # The choice of the conformity score depends on the problem we face.
 
-fig, axs = plt.subplots(2, 2, figsize=(16, 16))
+fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
-axs[0, 0].errorbar(
-    y_test,
-    y_pred_absconfscore,
-    yerr=yerr_absconfscore,
-    alpha=0.5,
-    linestyle="None",
-)
-axs[0, 0].scatter(y_test, y_pred_absconfscore, s=1, color="black")
-axs[0, 0].plot(
-    [0, max(max(y_test), max(y_pred_absconfscore))],
-    [0, max(max(y_test), max(y_pred_absconfscore))],
-    "-r",
-)
-axs[0, 0].set_xlabel("actual price [$]")
-axs[0, 0].set_ylabel("pred price [$]")
-axs[0, 0].grid()
-axs[0, 0].set_title(
-    f"AbsoluteResidualScore - coverage={coverage_absconfscore:.0%}"
-)
+for img_id, y_pred, y_err, cov, class_name, int_width in zip(
+    [0, 1],
+    [y_pred_absconfscore, y_pred_gammaconfscore],
+    [yerr_absconfscore, yerr_gammaconfscore],
+    [coverage_absconfscore, coverage_gammaconfscore],
+    ["AbsoluteResidualScore", "GammaResidualScore"],
+    [pred_int_width_absconfscore, pred_int_width_gammaconfscore],
+):
+    axs[0, img_id].errorbar(
+        y_test,
+        y_pred,
+        yerr=y_err,
+        alpha=0.5,
+        linestyle="None",
+    )
+    axs[0, img_id].scatter(y_test, y_pred, s=1, color="black")
+    axs[0, img_id].plot(
+        [0, max(max(y_test), max(y_pred))],
+        [0, max(max(y_test), max(y_pred))],
+        "-r",
+    )
+    axs[0, img_id].set_xlabel("Actual price [$]")
+    axs[0, img_id].set_ylabel("Predicted price [$]")
+    axs[0, img_id].grid()
+    axs[0, img_id].set_title(f"{class_name} - coverage={cov:.0%}")
 
-axs[0, 1].errorbar(
-    y_test,
-    y_pred_gammaconfscore,
-    yerr=yerr_gammaconfscore,
-    alpha=0.5,
-    linestyle="None",
-)
-axs[0, 1].scatter(y_test, y_pred_gammaconfscore, s=1, color="black")
-axs[0, 1].plot(
-    [0, max(max(y_test), max(y_pred_gammaconfscore))],
-    [0, max(max(y_test), max(y_pred_gammaconfscore))],
-    "-r",
-)
-axs[0, 1].set_xlabel("actual price [$]")
-axs[0, 1].set_ylabel("pred price [$]")
-axs[0, 1].grid()
-axs[0, 1].set_title(
-    f"GammaResidualScore - coverage={coverage_gammaconfscore:.0%}"
-)
+    xmin, xmax = axs[0, img_id].get_xlim()
+    ymin, ymax = axs[0, img_id].get_ylim()
+    axs[1, img_id].scatter(y_test, int_width, marker="+")
+    axs[1, img_id].set_xlabel("Actual price [$]")
+    axs[1, img_id].set_ylabel("Prediction interval width [$]")
+    axs[1, img_id].grid()
+    axs[1, img_id].set_xlim([xmin, xmax])
+    axs[1, img_id].set_ylim([ymin, ymax])
+
 fig.suptitle(
-    f"Predicted values with the confidence intervals of level {alpha}"
+    f"Predicted values with the prediction intervals of level {alpha}"
 )
-
-xmin, xmax = axs[0, 0].get_xlim()
-ymin, ymax = axs[0, 0].get_ylim()
-axs[1, 0].scatter(y_test, conf_int_width_absconfscore, marker="+")
-axs[1, 0].set_xlabel("actual price [$]")
-axs[1, 0].set_ylabel("confidence interval width [$]")
-axs[1, 0].grid()
-axs[1, 0].set_xlim([xmin, xmax])
-axs[1, 0].set_ylim([ymin, ymax])
-
-axs[1, 1].scatter(y_test, conf_int_width_gammaconfscore, marker="+")
-axs[1, 1].set_xlabel("actual price [$]")
-axs[1, 1].set_ylabel("confidence interval width [$]")
-axs[1, 1].grid()
-axs[1, 1].set_xlim([xmin, xmax])
-axs[1, 1].set_ylim([ymin, ymax])
-
+plt.subplots_adjust(wspace=0.3, hspace=0.3)
 plt.show()
