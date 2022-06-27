a8b3cfa85debc6c92008715462192c92c533709e
Vianney Taquet <vianney.taquet@quantmetry.com>
2022-06-27 11:13:20 +0200

    Bump version: 0.3.2 → 0.4.0

--- doc/conf.py
+++ doc/conf.py
@@ -87,7 +87,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "0.3.2"
+version = "0.4.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
