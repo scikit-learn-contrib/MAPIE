7a2ef0dcbd14f245a33e05749b3bd575531ef889
Thibault Cordier <tcordier@quantmetry.com>
2023-03-22 11:29:58 +0100

    Bump version: 0.6.1 → 0.6.2

--- doc/conf.py
+++ doc/conf.py
@@ -87,7 +87,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "0.6.1"
+version = "0.6.2"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 from setuptools import find_packages, setup
 
 DISTNAME = "MAPIE"
-VERSION = "0.6.1"
+VERSION = "0.6.2"
 DESCRIPTION = (
     "A scikit-learn-compatible module "
     "for estimating prediction intervals."
