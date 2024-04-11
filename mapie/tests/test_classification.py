91fec3155fc0061e421d1a32e7665cbbd9829cff
Baptiste Calot <baptiste.calot@capgemini.com>
2024-04-11 10:51:13 +0200

    FIX ligne qui dépasse les 79 caractères

--- mapie/tests/test_classification.py
+++ mapie/tests/test_classification.py
@@ -25,7 +25,11 @@ from mapie._typing import ArrayLike, NDArray
 from mapie.classification import MapieClassifier
 from mapie.metrics import classification_coverage_score
 from mapie.utils import check_alpha
-from mapie.conformity_scores.utils_classification_conformity_scores import get_true_label_position
+
+from mapie.conformity_scores.utils_classification_conformity_scores import (
+
+    get_true_label_position
+    )
 
 random_state = 42
 
