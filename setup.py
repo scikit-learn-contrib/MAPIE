faf31c6ae86483515d8b59674ee4ddca2c7f7472
Louis Lacombe <llacombe@quantmetry.com>
2024-04-19 16:37:42 +0200

    Update Python version classifiers in setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,9 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10"
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12"
 ]
 
 setup(
