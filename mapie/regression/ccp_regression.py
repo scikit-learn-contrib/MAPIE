4cf1a9f98f6354d4b130728b864addb37aba7368
Damien Bouet <bouet.dam@gmail.com>
2024-06-06 16:53:20 +0200

    RMV: Exemple from MapieCCPRegressor

--- mapie/regression/ccp_regression.py
+++ mapie/regression/ccp_regression.py
@@ -119,28 +119,6 @@ class MapieCCPRegressor(BaseEstimator, RegressorMixin):
     ----------
     Isaac Gibbs and John J. Cherian and Emmanuel J. Candès.
     "Conformal Prediction With Conditional Guarantees", 2023
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from mapie.regression import MapieCCPRegressor
-    >>> np.random.seed(1)
-    >>> X_train = np.arange(0,100,2).reshape(-1, 1)
-    >>> y_train = 2*X_train[:,0] + np.random.rand(len(X_train))
-    >>> mapie_reg = MapieCCPRegressor(alpha=0.1, random_state=1)
-    >>> mapie_reg = mapie_reg.fit(
-    ...     X_train,
-    ...     y_train,
-    ... )
-    >>> y_pred, y_pis = mapie_reg.predict(X_train)
-    >>> print(np.round(y_pred[:5], 2))
-    [ 0.43  4.43  8.43 12.43 16.43]
-    >>> print(np.round(y_pis[:5,:, 0], 2))
-    [[ 0.07  0.79]
-     [ 4.03  4.83]
-     [ 8.    8.86]
-     [11.98 12.89]
-     [15.97 16.9 ]]
     """
 
     default_sym_ = True
