313f6c76f6fbfc143d1de51a46cd1b3c436c4653
Thibault Cordier <tcordier@quantmetry.com>
2024-06-07 16:54:48 +0200

    Bump version: 0.8.3 → 0.8.4

--- doc/conf.py
+++ doc/conf.py
@@ -88,7 +88,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "0.8.3"
+version = "0.8.4"
 # The full version, including alpha/beta/rc tags.
 release = version
 
