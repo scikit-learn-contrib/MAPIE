dca25d37cac50e64182c783db48bd0d42af07ba4
Damien Bouet <bouet.dam@gmail.com>
2024-06-18 18:01:47 +0200

    ADD: test to check equivalence of new and old implementation of standard split CP

--- mapie/tests/test_standard_calibrator.py
+++ mapie/tests/test_standard_calibrator.py
@@ -4,9 +4,12 @@ import numpy as np
 import pytest
 from sklearn.datasets import make_regression
 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 from mapie.calibrators import StandardCalibrator
 from mapie.conformity_scores import AbsoluteConformityScore
-from mapie.regression import SplitCPRegressor
+from mapie.regression import SplitCPRegressor, MapieRegressor
 
 random_state = 1
 np.random.seed(random_state)
@@ -33,3 +36,25 @@ def test_calibrator_fit_predict(sym: bool) -> None:
                              conformity_score=AbsoluteConformityScore(sym=sym))
     mapie.fit(X, y, z=z)
     mapie.predict(X, z=z)
+
+
+def test_standard_equivalence() -> None:
+    """
+    Check that ``SplitCPRegressor`` with ``StandardCalibrator`` gives the
+    same results as ``MapieRegressor`` with ``method='base'``.
+    """
+    X_train, X_calib, y_train, y_calib = train_test_split(
+        X, y, test_size=0.5, random_state=1
+    )
+    predictor = LinearRegression().fit(X_train, y_train)
+    mapie_ccp = SplitCPRegressor(predictor, calibrator=StandardCalibrator(),
+                                 cv="prefit", alpha=0.1)
+    mapie_ccp.fit(X_calib, y_calib)
+    y_pred_ccp, y_pi_ccp = mapie_ccp.predict(X)
+
+    mapie_split = MapieRegressor(predictor, method="base", cv="prefit")
+    mapie_split.fit(X_calib, y_calib)
+    y_pred_split, y_pi_split = mapie_split.predict(X, alpha=0.1)
+
+    np.testing.assert_allclose(y_pred_ccp, y_pred_split)
+    np.testing.assert_allclose(y_pi_ccp, y_pi_split)
