43ed079abb321f42edafc539f0bcbb0d1209d369
Baptiste Calot <baptiste.calot@capgemini.com>
2024-07-03 16:14:53 +0200

    run isort

--- mapie/regression/regression.py
+++ mapie/regression/regression.py
@@ -13,12 +13,12 @@ from sklearn.utils.validation import _check_y, check_is_fitted, indexable
 
 from mapie._typing import ArrayLike, NDArray
 from mapie.conformity_scores import ConformityScore, ResidualNormalisedScore
-from mapie.estimator.regressor import EnsembleRegressor
-from mapie.utils import (check_alpha, check_alpha_and_n_samples,
-                         check_cv, check_estimator_fit_predict,
-                         check_n_features_in, check_n_jobs, check_null_weight,
-                         check_verbose, get_effective_calibration_samples)
 from mapie.conformity_scores.checks import check_conformity_score
+from mapie.estimator.regressor import EnsembleRegressor
+from mapie.utils import (check_alpha, check_alpha_and_n_samples, check_cv,
+                         check_estimator_fit_predict, check_n_features_in,
+                         check_n_jobs, check_null_weight, check_verbose,
+                         get_effective_calibration_samples)
 
 
 class MapieRegressor(BaseEstimator, RegressorMixin):
