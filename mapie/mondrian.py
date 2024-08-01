0ead65c7d71bbb0a965852092d07bd6b7a3d79ad
vincentblot28 <vincentblot28@gmail.com>
2024-08-01 19:28:03 +0200

    ADD: typing docstring and linting

--- mapie/mondrian.py
+++ mapie/mondrian.py
@@ -1,9 +1,10 @@
 from copy import deepcopy
-from typing import Union, cast
+from typing import Iterable, Optional, Tuple, Union, cast
 
 import numpy as np
 from sklearn.utils.validation import _check_y, check_is_fitted, indexable
 
+from mapie.calibration import MapieCalibrator
 from mapie.classification import MapieClassifier
 from mapie.conformity_scores import (
     AbsoluteConformityScore,
@@ -19,7 +20,6 @@ from mapie.utils import check_alpha
 from mapie._typing import ArrayLike, NDArray
 
 
-
 class Mondrian:
     """Mondrian is a method that allows to make  perform conformal predictions
     for disjoints groups of individuals.
@@ -31,18 +31,22 @@ class Mondrian:
 
     Parameters
     ----------
-    mapie_estimator : Union[MapieClassifier, MapieRegressor or MapieMultiLabelClassifier]
-        The estimator for which the Mondrian method will be applied. The estimator must 
-        be used with cv='prefit' and the conformity score must be one of the following:
-        - For MapieClassifier: 'lac', 'score', 'cumulated_score', 'aps' or 'topk'
+    mapie_estimator : Union[MapieClassifier, MapieRegressor,
+                            MapieMultiLabelClassifier]
+        The estimator for which the Mondrian method will be applied. The
+        estimator must be used with cv='prefit' and the conformity score must
+        be one of the following:
+        - For MapieClassifier: 'lac', 'score', 'cumulated_score',
+        'aps' or 'topk'
         - For MapieRegressor: 'gamma', 'absolute' or 'aps'
-    
+
     Attributes
     ----------
     unique_groups : NDArray
         The unique groups of individuals for which the estimator was fitted
     mapie_estimators : Dict
-        A dictionary containing the fitted conformal estimator for each group of individuals
+        A dictionary containing the fitted conformal estimator for each group
+        of individuals
 
     References
     ----------
@@ -59,7 +63,8 @@ class Mondrian:
     >>> y_toy = np.stack([0, 0, 1, 0, 1, 2, 1, 2, 2])
     >>> groups = [0, 0, 0, 0, 1, 1, 1, 1, 1]
     >>> clf = GaussianNB().fit(X_toy, y_toy)
-    >>> mapie = Mondrian(MapieClassifier(estimator=clf, cv="prefit")).fit(X_toy, y_toy, groups=groups)
+    >>> mapie = Mondrian(MapieClassifier(estimator=clf, cv="prefit")).fit(
+    ...     X_toy, y_toy, groups)
     >>> _, y_pi_mapie = mapie.predict(X_toy, alpha=0.4, groups=groups)
     >>> print(y_pi_mapie[:, :, 0])
     [[ True False False]
@@ -74,7 +79,10 @@ class Mondrian:
     """
 
     allowed_estimators = (
-        MapieClassifier, MapieRegressor, MapieMultiLabelClassifier
+        MapieClassifier,
+        MapieRegressor,
+        MapieMultiLabelClassifier,
+        MapieCalibrator
     )
     allowed_classification_ncs_str = [
         "lac", "score", "cumulated_score", "aps", "topk"
@@ -92,69 +100,181 @@ class Mondrian:
     ]
 
     def __init__(
-        self, mapie_estimator: Union[MapieClassifier, MapieRegressor, MapieMultiLabelClassifier]
+        self, mapie_estimator: Union[
+            MapieCalibrator,
+            MapieClassifier,
+            MapieRegressor,
+            MapieMultiLabelClassifier
+        ]
     ):
         self.mapie_estimator = mapie_estimator
 
     def _check_mapie_classifier(self):
-        if not self.mapie_estimator.cv == "prefit":
-            raise ValueError(
-                "Mondrian can only be used if the underlying Mapie estimator "+
-                "uses cv='prefit'"
-            )
+        """
+        Check that the underlying Mapie estimator uses cv='prefit'
+
+        Raises
+        ------
+        ValueError
+            If the underlying Mapie estimator does not use cv='prefit'
+            if the Mondrian method is not used with a MapieMultiLabelClassifier
+        NotFittedError
+            If the underlying Mapie estimator is not fitted and is the Mondrian
+            method is used with a MapieMultiLabelClassifier
+        """
+        if not isinstance(self.mapie_estimator, MapieMultiLabelClassifier):
+            if not self.mapie_estimator.cv == "prefit":
+                raise ValueError(
+                    "Mondrian can only be used if the underlying Mapie" +
+                    "estimator uses cv='prefit'."
+                )
+        else:
+            check_is_fitted(self.mapie_estimator.estimator)
 
     def _check_groups_fit(self, X: NDArray, groups: NDArray):
         """Check that each group is defined by an integer and check that there
-        are at least 2 individuals per group"""
+        are at least 2 individuals per group
+
+        Parameters
+        ----------
+        X : NDArray of shape (n_samples, n_features)
+            The input data
+        groups : NDArray of shape (n_samples,)
+
+        Raises
+        ------
+        ValueError
+            If the groups are not defined by integers
+            If there is less than 2 individuals per group
+            If the number of individuals in the groups is not equal to the
+            number of rows in X
+        """
         if not np.issubdtype(groups.dtype, np.integer):
             raise ValueError("The groups must be defined by integers")
         _, counts = np.unique(groups, return_counts=True)
         if np.min(counts) < 2:
             raise ValueError("There must be at least 2 individuals per group")
         if len(groups) != X.shape[0]:
-            raise ValueError("The number of individuals in the groups must be equal to the number of rows in X")
+            raise ValueError(
+                "The number of individuals in the groups must be equal" +
+                " to the number of rows in X"
+            )
 
-    def _check_groups_predict(self, X, groups):
-        """Check that there is no new group in the prediction"""
+    def _check_groups_predict(self, X: NDArray, groups: ArrayLike) -> NDArray:
+        """Check that there is no new group in the prediction and that
+        the number of individuals in the groups is equal to the number of
+        rows in X
+
+        Parameters
+        ----------
+        X : ArrayLike of shape (n_samples, n_features)
+            The input data
+        groups : ArrayLike of shape (n_samples,)
+
+        returns
+        -------
+        groups : NDArray of shape (n_samples,)
+            Groups of individuals
+
+        Raises
+        ------
+        ValueError
+            If there is a new group in the prediction
+            If the number of individuals in the groups is not equal to the
+            number of rows in X
+        """
+        groups = cast(NDArray, np.array(groups))
         if not np.all(np.isin(groups, self.unique_groups)):
             raise ValueError("There is a new group in the prediction")
         if len(groups) != X.shape[0]:
-            raise ValueError("The number of individuals in the groups must be equal to the number of rows in X")
-        
+            raise ValueError("The number of individuals in the groups must " +
+                             "be equal to the number of rows in X")
+        return groups
+
     def _check_estimator(self):
+        """
+        Check that the estimator is in the allowed_estimators
+
+        Raises
+        ------
+        ValueError
+            If the estimator is not in the allowed_estimators
+        """
         if not isinstance(self.mapie_estimator, self.allowed_estimators):
             raise ValueError(
-                "The estimator must be a MapieClassifier, MapieRegressor or MapieMultiLabelClassifier"
+                "The estimator must be a MapieClassifier, MapieRegressor or" +
+                " MapieMultiLabelClassifier"
             )
-    
+
     def _check_confomity_score(self):
+        """
+        Check that the conformity score is in allowed_classification_ncs_str
+        or allowed_classification_ncs_class if the estimator is MapieClassifier
+        or in the allowed_regression_ncs if the estimator is a MapieRegressor
+
+        Raises
+        ------
+        ValueError
+            If conformity score is not in the allowed_classification_ncs_str
+            or allowed_classification_ncs_class if the estimator is a
+            MapieClassifier or in the allowed_regression_ncs if the estimator
+            is a MapieRegressor
+        """
         if isinstance(self.mapie_estimator, MapieClassifier):
             if self.mapie_estimator.conformity_score is not None:
-                if self.mapie_estimator.conformity_score not in self.allowed_classification_ncs_class:
+                if self.mapie_estimator.conformity_score not in \
+                   self.allowed_classification_ncs_class:
                     raise ValueError(
-                        "The conformity score for the MapieClassifier must be one of "+
-                        f"{self.allowed_classification_ncs_class}"
+                        "The conformity score for the MapieClassifier must" +
+                        f" be one of {self.allowed_classification_ncs_class}"
                     )
             if self.mapie_estimator.method is not None:
-                if self.mapie_estimator.method not in self.allowed_classification_ncs_str:
+                if self.mapie_estimator.method not in \
+                   self.allowed_classification_ncs_str:
                     raise ValueError(
-                        "The conformity score for the MapieClassifier must be one of "+
-                        f"{self.allowed_classification_ncs_str}"
+                        "The conformity score for the MapieClassifier must " +
+                        f"be one of {self.allowed_classification_ncs_str}"
                     )
         elif isinstance(self.mapie_estimator, MapieRegressor):
             if self.mapie_estimator.conformity_score is not None:
-                if self.mapie_estimator.conformity_score not in self.allowed_regression_ncs:
+                if self.mapie_estimator.conformity_score not in\
+                      self.allowed_regression_ncs:
                     raise ValueError(
-                        "The conformity score for the MapieRegressor must be one of "+
-                        f"{self.allowed_regression_ncs}"
+                        "The conformity score for the MapieRegressor must " +
+                        f"be one of {self.allowed_regression_ncs}"
                     )
 
-    def _check_fit_parameters(self, X, y, groups):
+    def _check_fit_parameters(
+            self, X: ArrayLike, y: ArrayLike, groups: ArrayLike
+    ) -> Tuple[NDArray, NDArray, NDArray]:
+        """
+        Perform checks on the input data, groups and the estimator
+
+        Parameters
+        ----------
+        X : ArrayLike of shape (n_samples, n_features)
+            The input data
+        y : ArrayLike of shape (n_samples,) or (n_samples, n_outputs)
+            The target values
+        groups : ArrayLike of shape (n_samples,)
+            The groups of individuals
+
+        Returns
+        -------
+        X : NDArray of shape (n_samples, n_features)
+            The input data
+        y : NDArray of shape (n_samples,) or (n_samples, n_outputs)
+            The target values
+        groups : NDArray of shape (n_samples,)
+        """
         self._check_estimator()
         self._check_mapie_classifier()
         self._check_confomity_score()
         X, y = indexable(X, y)
-        y = _check_y(y)
+        if isinstance(self.mapie_estimator, MapieMultiLabelClassifier):
+            y = _check_y(y, multi_output=True)
+        else:
+            y = _check_y(y)
         X = cast(NDArray, X)
         y = cast(NDArray, y)
         groups = cast(NDArray, np.array(groups))
@@ -162,9 +282,46 @@ class Mondrian:
 
         return X, y, groups
 
+    def _check_is_topk_calibrator(self):
+        """
+        Check that the predict_proba method can only be used with a
+        MapieCalibrator estimator
+        """
+        if not isinstance(self.mapie_estimator, MapieCalibrator):
+            raise ValueError(
+                "The predict_proba method can only be used with a " +
+                "MapieCalibrator estimator"
+            )
+
+    def _check_not_topk_calibrator(self):
+        """
+        Check that the predict method can only be used with a MapieCalibrator
+        estimator
+        """
+        if isinstance(self.mapie_estimator, MapieCalibrator):
+            raise ValueError(
+                "The predict method can only be used with a MapieClassifier," +
+                "MapieRegressor or MapieMultiLabelClassifier estimator"
+            )
+
     def fit(self, X: ArrayLike, y: ArrayLike, groups: ArrayLike, **kwargs):
-        
-        self._check_fit_parameters(X, y, groups)
+        """
+        Fit the Mondrian method
+
+        Parameters
+        ----------
+        X : ArrayLike of shape (n_samples, n_features)
+            The input data
+        y : ArrayLike of shape (n_samples,) or (n_samples, n_outputs)
+            The target values
+        groups : ArrayLike of shape (n_samples,)
+            The groups of individuals
+        **kwargs
+            Additional keyword arguments to pass to the estimator's fit method
+            that may be specific to the Mapie estimator used
+        """
+
+        X, y, groups = self._check_fit_parameters(X, y, groups)
         self.unique_groups = np.unique(groups)
         self.mapie_estimators = {}
 
@@ -176,10 +333,40 @@ class Mondrian:
             self.mapie_estimators[group] = mapie_group_estimator
         return self
 
-    def predict(self, X: ArrayLike, alpha, groups, **kwargs):
+    def predict(
+            self, X: ArrayLike, groups: ArrayLike,
+            alpha: Optional[Union[float, Iterable[float]]] = None,
+            **kwargs
+    ) -> Union[NDArray, Tuple[NDArray, NDArray]]:
+        """
+        Perform conformal prediction for each group of individuals
+
+        Parameters
+        ----------
+        X : ArrayLike of shape (n_samples, n_features)
+            The input data
+        groups : ArrayLike of shape (n_samples,)
+            The groups of individuals
+        alpha : float or Iterable[float], optional
+            The desired coverage level(s) for each group.
+
+            By default None.
+        **kwargs
+            Additional keyword arguments to pass to the estimator's predict
+            method that may be specific to the Mapie estimator used
+
+        Returns
+        -------
+        y_pred : NDArray of shape (n_samples,) or (n_samples, n_outputs)
+            The predicted values
+        y_pss : NDArray of shape (n_samples, n_outputs, n_alpha)
+        """
 
         check_is_fitted(self, self.fit_attributes)
-        self._check_groups_predict(X, groups)
+        self._check_not_topk_calibrator()
+        X = indexable(X)
+        X = cast(NDArray, X)
+        groups = self._check_groups_predict(X, groups)
         if alpha is None:
             return self.mapie_estimator.predict(X, **kwargs)
         else:
@@ -188,14 +375,57 @@ class Mondrian:
             for i, group in enumerate(unique_groups):
                 indices_groups = np.argwhere(groups == group)[:, 0]
                 X_g = X[indices_groups]
-                y_pred_g, y_pss_g = self.mapie_estimators[group].predict(X_g, alpha=alpha_np, **kwargs)
+                y_pred_g, y_pss_g = self.mapie_estimators[group].predict(
+                    X_g, alpha=alpha_np, **kwargs
+                )
                 if i == 0:
                     if len(y_pred_g.shape) == 1:
                         y_pred = np.empty((X.shape[0],))
                     else:
                         y_pred = np.empty((X.shape[0], y_pred_g.shape[1]))
-                    y_pss = np.empty((X.shape[0], y_pss_g.shape[1], len(alpha_np)))
+                    y_pss = np.empty(
+                        (X.shape[0], y_pss_g.shape[1], len(alpha_np))
+                    )
                 y_pred[indices_groups] = y_pred_g
                 y_pss[indices_groups] = y_pss_g
 
             return y_pred, y_pss
+
+    def predict_proba(
+            self, X: ArrayLike, groups: ArrayLike, **kwargs
+    ) -> NDArray:
+        """
+        Perform top-label calibration for each group of individuals
+
+        Parameters
+        ----------
+        X : ArrayLike of shape (n_samples, n_features)
+            The input data
+        groups : ArrayLike of shape (n_samples,)
+            The groups of individuals
+        **kwargs
+            Additional keyword arguments to pass to the estimator's
+            predict_proba method that may be specific to the Mapie estimator
+            used
+
+        Returns
+        -------
+        y_pred_proba : NDArray of shape (n_samples, n_classes)
+            The calibrated predicted probabilities
+        """
+        self._check_is_topk_calibrator()
+        X = indexable(X)
+        X = cast(NDArray, X)
+        unique_groups = np.unique(groups)
+        y_pred_proba = np.empty(
+            (X.shape[0], len(self.mapie_estimator.estimator.classes_))
+        )
+        for group in unique_groups:
+            indices_groups = np.argwhere(groups == group)[:, 0]
+            X_g = X[indices_groups]
+            y_pred_proba_g = self.mapie_estimators[group].predict_proba(
+                X_g, **kwargs
+            )
+            y_pred_proba[indices_groups] = y_pred_proba_g
+
+        return y_pred_proba
