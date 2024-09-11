bcf283e16f4d2bd365d2b8d47283f3d3a93f0193
vincentblot28 <vincentblot28@gmail.com>
2024-09-11 10:53:25 +0200

    FIX: change warn place in file

--- mapie/conformity_scores/conformity_scores.py
+++ mapie/conformity_scores/conformity_scores.py
@@ -1,8 +1,10 @@
-from .regression import BaseRegressionScore as ConformityScore  # noqa: F401
-
 import warnings
 warnings.warn(
     "Conformity score class is depreciated. Prefer import " +
     "BaseRegressionScore from mapie.conformity_scores",
     DeprecationWarning
+)
+
+from .regression import (  # noqa: F401, E402
+    BaseRegressionScore as ConformityScore
 )
