ba2546539d9a43371f11a46e905170888e2a2b5d
vincentblot28 <vincentblot28@gmail.com>
2024-09-13 09:55:29 +0200

    Bump version: 0.9.0 → 0.9.1

--- doc/conf.py
+++ doc/conf.py
@@ -88,7 +88,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "0.9.0"
+version = "0.9.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
