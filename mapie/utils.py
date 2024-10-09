af1149240fc4d4732f6daba733020ae0a801a6a4
sd29206 <hussein.jawad@external.stellantis.com>
2024-10-09 15:04:03 +0200

    v1 branch - initial commit (made of a bunch of wip commits squashed together)

--- mapie/utils.py
+++ mapie/utils.py
@@ -75,6 +75,8 @@ def check_null_weight(
     return sample_weight, X, y
 
 
+# TODO back-end: this will be useless in v1 because we'll not distinguish
+# sample_weight from other fit_params
 def fit_estimator(
     estimator: Union[RegressorMixin, ClassifierMixin],
     X: ArrayLike,
