16d00df07337b78348c7e1a3023e3395d3b099c6
jawadhussein462 <jawadhussein462@gmail.com>
2024-11-13 11:26:21 +0100

    add test_exact_interval_equality

--- mapie_v1/conformity_scores/utils.py
+++ mapie_v1/conformity_scores/utils.py
@@ -6,17 +6,19 @@ from mapie.conformity_scores import (
     ResidualNormalisedScore,
 )
 
+CONFORMITY_SCORES_STRING_MAP = {
+    "absolute": AbsoluteConformityScore,
+    "gamma": GammaConformityScore,
+    "residual_normalized": ResidualNormalisedScore,
+}
+
 
 def select_conformity_score(
     conformity_score: Union[str, BaseRegressionScore]
 ) -> BaseRegressionScore:
     if isinstance(conformity_score, BaseRegressionScore):
         return conformity_score
-    elif conformity_score == "absolute":
-        return AbsoluteConformityScore()
-    elif conformity_score == "gamma":
-        return GammaConformityScore()
-    elif conformity_score == "residualsNorm":
-        return ResidualNormalisedScore()
+    elif conformity_score in CONFORMITY_SCORES_STRING_MAP:
+        return CONFORMITY_SCORES_STRING_MAP[conformity_score]()
     else:
         raise ValueError("Invalid conformity_score type")
--- mapie_v1/integration_tests/tests/test_regression.py
+++ mapie_v1/integration_tests/tests/test_regression.py
@@ -6,7 +6,6 @@ from numpy.testing import assert_almost_equal
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import KFold, train_test_split
 
 from mapie_v1.regression import (
     SplitConformalRegressor,
@@ -17,75 +16,102 @@ from mapie_v1.regression import (
 from mapiev0.regression import MapieRegressor as MapieRegressorV0 # noqa
 from mapiev0.regression import MapieQuantileRegressor as MapieQuantileRegressorV0 # noqa
 from mapie_v1.conformity_scores.utils import select_conformity_score
-from mapie_v1.utils import filter_params
+from mapie_v1.utils import (filter_params,
+                            calculate_coverage,
+                            train_test_split_shuffle)
+from sklearn.model_selection import KFold
 
 # Constants
 RANDOM_STATE = 1
+K_FOLDS = 3
+N_BOOTSTRAPS = 30
+
 X_toy = np.array([0, 1, 2, 3, 4, 5]).reshape(-1, 1)
 y_toy = np.array([5, 7, 9, 11, 13, 15])
 X, y = make_regression(n_samples=500,
                        n_features=10,
                        noise=1.0,
                        random_state=RANDOM_STATE)
-K_FOLDS = 3
-n_bootstraps = 30
 
 
-def calculate_coverage(y_true, pred_intervals):
-    return np.mean((y_true >= pred_intervals[:, 0]) &
-                   (y_true <= pred_intervals[:, 1]))
-
-
-def initialize_models(
+@pytest.mark.parametrize("strategy_key", ["split"])
+@pytest.mark.parametrize("method", ["base", "plus", "minmax"])
+@pytest.mark.parametrize("conformity_score", ["absolute"])
+@pytest.mark.parametrize("confidence_level", [0.9, 0.95, 0.99])
+@pytest.mark.parametrize("agg_function", ["mean", "median"])
+@pytest.mark.parametrize(
+    "estimator", [
+        LinearRegression(),
+        RandomForestRegressor(random_state=RANDOM_STATE, max_depth=2)])
+@pytest.mark.parametrize("test_size", [0.2, 0.5])
+def test_exact_interval_equality(
     strategy_key,
-    v0_params: dict,
-    v1_params: dict,
-    k_folds=5,
-    random_state=42
+    method,
+    conformity_score,
+    confidence_level,
+    agg_function,
+    estimator,
+    test_size
 ):
+    """
+    Test that the prediction intervals are exactly the same
+    between v0 and v1 models when using the same settings.
+    """
 
-    # Apply the relevant parameters to v0 based on strategy_key
-    if strategy_key == "prefit":
-        v0_params.update({"cv": "prefit"})
-        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
-        v1_params = filter_params(SplitConformalRegressor.__init__, v1_params)
-        v0 = MapieRegressorV0(**v0_params)
-        v1 = SplitConformalRegressor(prefit=True, **v1_params)
+    # Define parameters for v0 and v1 versions of the model
+    v0_params = {
+        "estimator": estimator,
+        "method": method,
+        "conformity_score": select_conformity_score(conformity_score),
+        "alpha": 1 - confidence_level,
+        "agg_function": agg_function,
+        "random_state": RANDOM_STATE,
+        "test_size": test_size,
+    }
+    v1_params = {
+        "estimator": estimator,
+        "method": method,
+        "conformity_score": conformity_score,
+        "confidence_level": confidence_level,
+        "aggregate_function": agg_function,
+        "random_state": RANDOM_STATE,
+        "n_bootstraps": N_BOOTSTRAPS,
+    }
 
-    elif strategy_key == "split":
-        v0_params.update({"cv": "split"})
-        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
-        v1_params = filter_params(SplitConformalRegressor.__init__, v1_params)
-        v0 = MapieRegressorV0(**v0_params)
-        v1 = SplitConformalRegressor(**v1_params)
+    # Initialize models based on the chosen strategy
+    v0, v1 = initialize_models(
+        strategy_key=strategy_key,
+        v0_params=v0_params,
+        v1_params=v1_params,
+        k_folds=K_FOLDS,
+        random_state=RANDOM_STATE
+    )
 
-    elif strategy_key == "cv":
-        v0_params.update({"cv": KFold(n_splits=k_folds,
-                                      shuffle=True,
-                                      random_state=RANDOM_STATE)})
-        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
-        v1_params = filter_params(CrossConformalRegressor.__init__, v1_params)
-        v0 = MapieRegressorV0(**v0_params)
-        v1 = CrossConformalRegressor(cv=k_folds, **v1_params)
+    # Split the data into training and conformity sets
+    X_train, X_conf, y_train, y_conf = train_test_split_shuffle(
+        X, y, test_size=test_size, random_state=RANDOM_STATE
+    )
 
-    elif strategy_key == "jackknife":
-        v0_params.update({"cv": -1})
-        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
-        v1_params = filter_params(JackknifeAfterBootstrapRegressor.__init__,
-                                  v1_params)
-        v0 = MapieRegressorV0(**v0_params)
-        v1 = JackknifeAfterBootstrapRegressor(**v1_params)
+    # Fit v0 and v1
+    v0.fit(X, y)
+    v1.fit(X_train, y_train)
 
-    elif strategy_key == "CQR":
-        v0_params = filter_params(MapieQuantileRegressorV0.__init__, v0_params)
-        v1_params = filter_params(SplitConformalRegressor.__init__, v1_params)
-        v0 = MapieQuantileRegressorV0(**v0_params)
-        v1 = ConformalizedQuantileRegressor(**v1_params)
+    # Calibration
+    v1.conformalize(X_conf, y_conf)
 
-    else:
-        raise ValueError(f"Unknown strategy key: {strategy_key}")
+    # Predict intervals with v0 and v1
+    v0_predict_params = filter_params(v0.predict, v0_params)
+    v1_predict_params = filter_params(v1.predict, v1_params)
+    _, v0_pred_intervals = v0.predict(X_conf, **v0_predict_params)
+    v1_pred_intervals = v1.predict_set(X_conf, **v1_predict_params)
+    v0_pred_intervals = v0_pred_intervals[:, :, 0]
 
-    return v0, v1
+    # Assert that intervals are exactly equal
+    np.testing.assert_array_equal(
+        v1_pred_intervals,
+        v0_pred_intervals,
+        err_msg="Prediction intervals differ between v0 and v1 models"
+    )
 
 
 @pytest.mark.parametrize("strategy_key", ["split"])
@@ -122,7 +148,7 @@ def test_consistent_coverage(
         "confidence_level": confidence_level,
         "aggregate_function": agg_function,
         "random_state": RANDOM_STATE,
-        "n_bootstraps": n_bootstraps,
+        "n_bootstraps": N_BOOTSTRAPS,
     }
 
     v0, v1 = initialize_models(
@@ -134,7 +160,7 @@ def test_consistent_coverage(
     )
 
     # Train/test split
-    X_train, X_conf, y_train, y_conf = train_test_split(
+    X_train, X_conf, y_train, y_conf = train_test_split_shuffle(
         X, y, test_size=test_size, random_state=RANDOM_STATE)
 
     # Fit v0 and v1
@@ -204,7 +230,7 @@ def test_consistent_coverage_for_prefit_model(
         "conformity_score": conformity_score,
         "confidence_level": confidence_level,
         "random_state": RANDOM_STATE,
-        "n_bootstraps": n_bootstraps,
+        "n_bootstraps": N_BOOTSTRAPS,
     }
 
     # Initialize models with 'prefit' strategy
@@ -217,7 +243,7 @@ def test_consistent_coverage_for_prefit_model(
     )
 
     # Split the data for training and conformity
-    X_train, X_conf, y_train, y_conf = train_test_split(
+    X_train, X_conf, y_train, y_conf = train_test_split_shuffle(
         X, y, test_size=test_size, random_state=RANDOM_STATE)
 
     # Fit the estimator initially
@@ -262,8 +288,9 @@ def test_consistent_coverage_for_prefit_model(
 @pytest.mark.parametrize("conformity_score", ["absolute"])
 @pytest.mark.parametrize("confidence_level", [0.9, 0.95, 0.99])
 @pytest.mark.parametrize("agg_function", ["mean", "median"])
-@pytest.mark.parametrize("estimator", [LinearRegression(),
-                                       RandomForestRegressor(max_depth=2)])
+@pytest.mark.parametrize("estimator", [
+        LinearRegression(),
+        RandomForestRegressor(random_state=RANDOM_STATE, max_depth=2)])
 @pytest.mark.parametrize("test_size", [0.2, 0.5])
 def test_consistent_interval_width(
     strategy_key,
@@ -297,7 +324,7 @@ def test_consistent_interval_width(
         "confidence_level": confidence_level,
         "aggregate_function": agg_function,
         "random_state": RANDOM_STATE,
-        "n_bootstraps": n_bootstraps,
+        "n_bootstraps": N_BOOTSTRAPS,
     }
 
     # Initialize models based on the chosen strategy
@@ -310,7 +337,7 @@ def test_consistent_interval_width(
     )
 
     # Split the data into training and conformity sets
-    X_train, X_conf, y_train, y_conf = train_test_split(
+    X_train, X_conf, y_train, y_conf = train_test_split_shuffle(
         X, y, test_size=test_size, random_state=RANDOM_STATE
     )
 
@@ -327,7 +354,7 @@ def test_consistent_interval_width(
     _, v0_pred_intervals = v0.predict(X_conf, **v0_predict_params)
     v1_pred_intervals = v1.predict_set(X_conf, **v1_predict_params)
     v0_pred_intervals = v0_pred_intervals[:, :, 0]
-    
+
     # Calculate interval widths
     v0_interval_widths = v0_pred_intervals[:, 1] - v0_pred_intervals[:, 0]
     v1_interval_widths = v1_pred_intervals[:, 1] - v1_pred_intervals[:, 0]
@@ -361,7 +388,7 @@ def test_dummy():
     _, v0_pred_intervals = v0.predict(X_toy, alpha=alpha)
     v0_pred_intervals = v0_pred_intervals[:, :, 0]
 
-    X_train, X_conf, y_train, y_conf = train_test_split(
+    X_train, X_conf, y_train, y_conf = train_test_split_shuffle(
         X_toy, y_toy, test_size=test_size, random_state=random_state
     )
     v1 = SplitConformalRegressor(
@@ -374,3 +401,55 @@ def test_dummy():
 
     np.testing.assert_array_equal(v1_preds, v0_preds)
     np.testing.assert_array_equal(v1_pred_intervals, v0_pred_intervals)
+
+
+def initialize_models(
+    strategy_key,
+    v0_params: dict,
+    v1_params: dict,
+    k_folds=5,
+    random_state=42
+):
+
+    # Apply the relevant parameters to v0 based on strategy_key
+    if strategy_key == "prefit":
+        v0_params.update({"cv": "prefit"})
+        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
+        v1_params = filter_params(SplitConformalRegressor.__init__, v1_params)
+        v0 = MapieRegressorV0(**v0_params)
+        v1 = SplitConformalRegressor(prefit=True, **v1_params)
+
+    elif strategy_key == "split":
+        v0_params.update({"cv": "split"})
+        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
+        v1_params = filter_params(SplitConformalRegressor.__init__, v1_params)
+        v0 = MapieRegressorV0(**v0_params)
+        v1 = SplitConformalRegressor(**v1_params)
+
+    elif strategy_key == "cv":
+        v0_params.update({"cv": KFold(n_splits=k_folds,
+                                      shuffle=True,
+                                      random_state=random_state)})
+        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
+        v1_params = filter_params(CrossConformalRegressor.__init__, v1_params)
+        v0 = MapieRegressorV0(**v0_params)
+        v1 = CrossConformalRegressor(cv=k_folds, **v1_params)
+
+    elif strategy_key == "jackknife":
+        v0_params.update({"cv": -1})
+        v0_params = filter_params(MapieRegressorV0.__init__, v0_params)
+        v1_params = filter_params(JackknifeAfterBootstrapRegressor.__init__,
+                                  v1_params)
+        v0 = MapieRegressorV0(**v0_params)
+        v1 = JackknifeAfterBootstrapRegressor(**v1_params)
+
+    elif strategy_key == "CQR":
+        v0_params = filter_params(MapieQuantileRegressorV0.__init__, v0_params)
+        v1_params = filter_params(SplitConformalRegressor.__init__, v1_params)
+        v0 = MapieQuantileRegressorV0(**v0_params)
+        v1 = ConformalizedQuantileRegressor(**v1_params)
+
+    else:
+        raise ValueError(f"Unknown strategy key: {strategy_key}")
+
+    return v0, v1
