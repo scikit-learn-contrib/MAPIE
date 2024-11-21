d1e9522f93fdb142c3deb82c3c1d3a77ce3628fe
Valentin Laurent <valentin.laurent.fr@gmail.com>
2024-11-21 10:31:41 +0100

    DOC: reorganize documentation, add migration_guide in doc in .rst format, make left navigation bar more usable (#540)

--- doc/conf.py
+++ doc/conf.py
@@ -146,7 +146,9 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
