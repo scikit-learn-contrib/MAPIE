8406831443099fb550263e23de554106f238724e
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-02-14 10:44:14 +0100

    REFACTO: add classification to conformity_score checks

--- mapie_v1/conformity_scores/__init__.py
+++ mapie_v1/conformity_scores/__init__.py
@@ -2,10 +2,24 @@ from mapie.conformity_scores import (
     AbsoluteConformityScore,
     GammaConformityScore,
     ResidualNormalisedScore,
+    LACConformityScore,
+    TopKConformityScore,
+    APSConformityScore,
+    RAPSConformityScore,
+    BaseRegressionScore,
+    BaseClassificationScore,
 )
 
-REGRESSION_CONFORMITY_SCORES_STRING_MAP = {
-    "absolute": AbsoluteConformityScore,
-    "gamma": GammaConformityScore,
-    "residual_normalized": ResidualNormalisedScore,
+CONFORMITY_SCORES_STRING_MAP = {
+    BaseRegressionScore: {
+        "absolute": AbsoluteConformityScore,
+        "gamma": GammaConformityScore,
+        "residual_normalized": ResidualNormalisedScore,
+    },
+    BaseClassificationScore: {
+        "lac": LACConformityScore,
+        "top_k": TopKConformityScore,
+        "aps": APSConformityScore,
+        "raps": RAPSConformityScore,
+    },
 }
--- mapie_v1/conformity_scores/_utils.py
+++ mapie_v1/conformity_scores/_utils.py
@@ -1,14 +1,20 @@
-from typing import Union
-from mapie.conformity_scores import BaseRegressionScore
-from . import REGRESSION_CONFORMITY_SCORES_STRING_MAP
+from typing import Union, TypeVar, Type
+from mapie.conformity_scores.interface import BaseConformityScore
+from . import CONFORMITY_SCORES_STRING_MAP
+
+RegressionOrClassificationScore = TypeVar(
+    'RegressionOrClassificationScore',
+    bound=BaseConformityScore
+)
 
 
-def check_and_select_regression_conformity_score(
-    conformity_score: Union[str, BaseRegressionScore]
-) -> BaseRegressionScore:
-    if isinstance(conformity_score, BaseRegressionScore):
+def check_and_select_conformity_score(
+    conformity_score: Union[str, RegressionOrClassificationScore],
+    conformity_score_type: Type[RegressionOrClassificationScore]
+) -> RegressionOrClassificationScore:
+    if isinstance(conformity_score, conformity_score_type):
         return conformity_score
-    elif conformity_score in REGRESSION_CONFORMITY_SCORES_STRING_MAP:
-        return REGRESSION_CONFORMITY_SCORES_STRING_MAP[conformity_score]()
+    elif conformity_score in CONFORMITY_SCORES_STRING_MAP[conformity_score_type]:
+        return CONFORMITY_SCORES_STRING_MAP[conformity_score_type][conformity_score]()
     else:
-        raise ValueError("Invalid conformity_score type")
+        raise ValueError("Invalid conformity_score parameter")
