24681b3ef8d68634bd1f2609bc17675be795f78c
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-02-21 14:09:11 +0100

    ENH: implement SplitConformalClassifier, fix and enhance corresponding integration tests

--- tests_v1/integration_tests/tests/test_classification.py
+++ tests_v1/integration_tests/tests/test_classification.py
@@ -6,6 +6,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 from mapie.classification import MapieClassifier
+from mapie.conformity_scores import (
+    RAPSConformityScore,
+    APSConformityScore,
+    TopKConformityScore, LACConformityScore,
+)
 from mapie_v1.classification import SplitConformalClassifier
 from tests_v1.integration_tests.utils import (
     DummyClassifierWithFitAndPredictParams,
@@ -21,26 +26,49 @@ RANDOM_STATE = 1
 @pytest.fixture(scope="module")
 def dataset():
     X, y = make_classification(
-        n_samples=200,
+        n_samples=1000,
         n_informative=5,
         n_classes=4,
         random_state=RANDOM_STATE
     )
+    sample_weight = RandomState(RANDOM_STATE).random(len(X))
+
+    (
+        X_train,
+        X_conformalize,
+        y_train,
+        y_conformalize,
+        sample_weight_train,
+        sample_weight_conformalize,
+    ) = train_test_split_shuffle(
+        X, y, random_state=RANDOM_STATE, sample_weight=sample_weight
+    )
+
     return {
         "X": X,
         "y": y,
-        "sample_weight": RandomState(RANDOM_STATE).random(len(X)),
+        "sample_weight": sample_weight,
+        "X_train": X_train,
+        "X_conformalize": X_conformalize,
+        "y_train": y_train,
+        "y_conformalize": y_conformalize,
+        "sample_weight_train": sample_weight_train,
+        "sample_weight_conformalize": sample_weight_conformalize,
     }
 
 
 @pytest.fixture()
-def params_split_test_1(dataset):
+def params_split_test_1():
     return {
-        "v1": {},
+        "v1": {
+            "__init__": {
+                "estimator": LogisticRegression(),
+            },
+        },
         "v0": {
             "__init__": {
                 "estimator": LogisticRegression(),
-                "conformity_score": "lac",
+                "conformity_score": LACConformityScore(),
                 "cv": "prefit"
             },
             "predict": {
@@ -49,7 +77,7 @@ def params_split_test_1(dataset):
 
 
 @pytest.fixture()
-def params_split_test_2(dataset):
+def params_split_test_2():
     return {
         "v1": {
             "__init__": {
@@ -57,25 +85,28 @@ def params_split_test_2(dataset):
                 "confidence_level": 0.8,
                 "prefit": False,
                 "conformity_score": "top_k",
+                "random_state": RANDOM_STATE,
             },
             "fit": {
-                "fit_params": {"fit_param": True},
+                "fit_params": {"dummy_fit_param": True},
             },
             "conformalize": {
-                "predict_params": {"predict_param": True},
+                "predict_params": {"dummy_predict_param": True},
             }},
         "v0": {
             "__init__": {
                 "estimator": DummyClassifierWithFitAndPredictParams(),
-                "conformity_score": "top_k",
-                "cv": "split"
+                "conformity_score": TopKConformityScore(),
+                "cv": "split",
+                "random_state": RANDOM_STATE,
             },
             "fit": {
-                "fit_params": {"fit_param": True},
+                "fit_params": {"dummy_fit_param": True},
+                "predict_params": {"dummy_predict_param": True},
             },
             "predict": {
                 "alpha": 0.2,
-                "predict_params": {"predict_param": True},
+                "dummy_predict_param": True,
             }}}
 
 
@@ -84,22 +115,24 @@ def params_split_test_3(dataset):
     return {
         "v1": {
             "__init__": {
-                "estimator": RandomForestClassifier(),
+                "estimator": RandomForestClassifier(random_state=RANDOM_STATE),
                 "confidence_level": [0.8, 0.9],
                 "prefit": False,
                 "conformity_score": "aps",
+                "random_state": RANDOM_STATE,
             },
             "fit": {
-                "fit_params": {"sample_weight": dataset["sample_weight"]},
+                "fit_params": {"sample_weight": dataset["sample_weight_train"]},
             },
             "predict_set": {
                 "conformity_score_params": {"include_last_label": False}
             }},
         "v0": {
             "__init__": {
-                "estimator": RandomForestClassifier(),
-                "conformity_score": "aps",
-                "cv": "split"
+                "estimator": RandomForestClassifier(random_state=RANDOM_STATE),
+                "conformity_score": APSConformityScore(),
+                "cv": "split",
+                "random_state": RANDOM_STATE,
             },
             "fit": {
                 "sample_weight": dataset["sample_weight"],
@@ -111,17 +144,41 @@ def params_split_test_3(dataset):
 
 
 @pytest.fixture()
-def params_split_test_4(dataset):
+def params_split_test_4():
     return {
         "v1": {
             "__init__": {
+                "estimator": LogisticRegression(),
                 "conformity_score": "raps",
+                "random_state": RANDOM_STATE,
             }},
         "v0": {
             "__init__": {
                 "estimator": LogisticRegression(),
-                "conformity_score": "raps",
-                "cv": "prefit"
+                "conformity_score": RAPSConformityScore(),
+                "cv": "prefit",
+                "random_state": RANDOM_STATE,
+            },
+            "predict": {
+                "alpha": 0.1,
+            }}}
+
+
+@pytest.fixture()
+def params_split_test_5():
+    return {
+        "v1": {
+            "__init__": {
+                "estimator": LogisticRegression(),
+                "conformity_score": RAPSConformityScore(size_raps=0.4),
+                "random_state": RANDOM_STATE,
+            }},
+        "v0": {
+            "__init__": {
+                "estimator": LogisticRegression(),
+                "conformity_score": RAPSConformityScore(size_raps=0.4),
+                "cv": "prefit",
+                "random_state": RANDOM_STATE,
             },
             "predict": {
                 "alpha": 0.1,
@@ -133,44 +190,62 @@ def params_split_test_4(dataset):
         "params_split_test_1",
         "params_split_test_2",
         "params_split_test_3",
-        "params_split_test_4"
+        "params_split_test_4",
+        "params_split_test_5",
     ]
 )
 def test_intervals_and_predictions_exact_equality_split(dataset, params, request):
+    X, y, X_train, X_conformalize, y_train, y_conformalize = (
+        dataset["X"],
+        dataset["y"],
+        dataset["X_train"],
+        dataset["X_conformalize"],
+        dataset["y_train"],
+        dataset["y_conformalize"],
+    )
+
     params_ = request.getfixturevalue(params)
-    v0_params = params_["v0"]
-    v1_params = params_["v1"]
 
-    X, y = dataset["X"], dataset["y"]
-    X_train, X_conformalize, y_train, y_conformalize = train_test_split_shuffle(
-        X, y, random_state=RANDOM_STATE
-    )
+    v0_init_params = params_["v0"].get("__init__", {})
+    v0_fit_params = params_["v0"].get("fit", {})
+    v0_predict_params = params_["v0"].get("predict", {})
 
-    v0 = MapieClassifier(**v0_params.get("__init__", {}))
-    v1 = SplitConformalClassifier(**v1_params.get("__init__", {}))
+    v1_init_params = params_["v1"].get("__init__", {})
+    v1_fit_params = params_["v1"].get("fit", {})
+    v1_conformalize_params = params_["v1"].get("conformalize", {})
+    v1_predict_set_params = params_["v1"].get("predict_set", {})
 
-    if v1_params.get("__init__", {}).get("prefit", True):
-        v0_params["estimator"].fit(X_train, y_train)
-        v1_params["estimator"].fit(X_train, y_train)
+    prefit = v1_init_params.get("prefit", True)
+
+    if prefit:
+        v0_init_params["estimator"].fit(X_train, y_train)
+        v1_init_params["estimator"].fit(X_train, y_train)
+
+    v0 = MapieClassifier(**v0_init_params)
+    v1 = SplitConformalClassifier(**v1_init_params)
+
+    if prefit:
+        v0.fit(X_conformalize, y_conformalize, **v0_fit_params)
     else:
-        v0.fit(X_train, y_train)
-        v1.fit(X_train, y_train)
+        v0.fit(X, y, **v0_fit_params)
+        v1.fit(X_train, y_train, **v1_fit_params)
+    v1.conformalize(X_conformalize, y_conformalize, **v1_conformalize_params)
 
-    v1.conformalize(X_conformalize, y_conformalize, **v1_params.get("conformalize", {}))
-
-    v0_preds, v0_pred_sets = v0.predict(X_conformalize, **v0_params.get("predict", {}))
-    v1_preds, v1_pred_sets = v1.predict_set(
-        X_conformalize,
-        **v1_params.get("predict_set", {})
-    )
+    v0_preds, v0_pred_sets = v0.predict(X_conformalize, **v0_predict_params)
+    v1_preds, v1_pred_sets = v1.predict_set(X_conformalize, **v1_predict_set_params)
 
     v1_preds_using_predict: ArrayLike = v1.predict(X_conformalize)
 
     np.testing.assert_array_equal(v0_preds, v1_preds)
     np.testing.assert_array_equal(v0_pred_sets, v1_pred_sets)
     np.testing.assert_array_equal(v1_preds_using_predict, v1_preds)
+
+    confidence_level = v1_init_params.get("confidence_level", 0.9)
+    n_confidence_level = 1 if isinstance(confidence_level, float) else len(
+        confidence_level
+    )
     assert v1_pred_sets.shape == (
         len(X_conformalize),
         len(np.unique(y)),
-        len(v1_params.get("confidence_level", [0.9]))
+        n_confidence_level,
     )
