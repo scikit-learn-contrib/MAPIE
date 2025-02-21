cabcacd3c84098bc9504a1eb6fe43ffca85deda2
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-02-21 14:09:11 +0100

    ENH: implement SplitConformalClassifier, fix and enhance corresponding integration tests

--- mapie_v1/classification.py
+++ mapie_v1/classification.py
@@ -4,12 +4,19 @@ from typing import Optional, Union, Tuple, Iterable
 from typing_extensions import Self
 
 import numpy as np
-from sklearn.base import ClassifierMixin
+from sklearn.base import ClassifierMixin, clone
 from sklearn.model_selection import BaseCrossValidator
 from sklearn.linear_model import LogisticRegression
 
 from mapie._typing import ArrayLike, NDArray
+from mapie.classification import MapieClassifier
 from mapie.conformity_scores import BaseClassificationScore
+from mapie_v1._utils import (
+    transform_confidence_level_to_alpha_list,
+    prepare_params,
+    cast_predictions_to_ndarray_tuple, cast_point_predictions_to_ndarray,
+)
+from mapie_v1.conformity_scores._utils import check_and_select_conformity_score
 
 
 class SplitConformalClassifier:
@@ -30,7 +37,28 @@ class SplitConformalClassifier:
         for splitting the conformalization set. Future implementations may allow the use
         of groups.
         """
-        pass
+        self._estimator = estimator
+        self._alphas = transform_confidence_level_to_alpha_list(
+            confidence_level
+        )
+        self._conformity_score = check_and_select_conformity_score(
+            conformity_score,
+            BaseClassificationScore
+        )
+        self._prefit = prefit
+
+        # Note to developers: to implement this v1 class without touching the
+        # v0 backend, we're for now using a hack. We always set cv="prefit",
+        # and we fit the estimator if needed. See the .fit method below.
+        self._mapie_classifier = MapieClassifier(
+            estimator=self._estimator,
+            cv="prefit",
+            n_jobs=n_jobs,
+            verbose=verbose,
+            conformity_score=self._conformity_score,
+            random_state=random_state,
+        )
+        self._predict_params: dict = {}
 
     def fit(
         self,
@@ -38,6 +66,14 @@ class SplitConformalClassifier:
         y_train: ArrayLike,
         fit_params: Optional[dict] = None,
     ) -> Self:
+        # Known issue to the following hack: we do not perform extensive checks,
+        # as they are done in _mapie_classifier.fit. Those checks are probably
+        # too extensive given MAPIE maturity anyway.
+        if not self._prefit:
+            cloned_estimator = clone(self._estimator)
+            fit_params_ = prepare_params(fit_params)
+            cloned_estimator.fit(X_train, y_train, **fit_params_)
+            self._mapie_classifier.estimator = cloned_estimator
         return self
 
     def conformalize(
@@ -46,10 +82,17 @@ class SplitConformalClassifier:
         y_conformalize: ArrayLike,
         predict_params: Optional[dict] = None,
     ) -> Self:
+        """
+        Specify that predict_params are passed to predict AND predict_proba,
+        and are used in .conformalize but also in .predict_set and .predict
+        """
+        self._predict_params = prepare_params(predict_params)
+        self._mapie_classifier.fit(
+            X_conformalize,
+            y_conformalize,
+            predict_params=self._predict_params,
+        )
         return self
-
-    def predict(self, X: ArrayLike) -> NDArray:
-        return np.ndarray(0)
 
     def predict_set(
         self,
@@ -57,11 +100,27 @@ class SplitConformalClassifier:
         conformity_score_params: Optional[dict] = None,
         # Prediction time parameters specific to conformity scores,
         # The only example for now is: include_last_label
+        # Add the doc of include_last_label to the docstring
     ) -> Tuple[NDArray, NDArray]:
         """
-        Shape: (n, ), (n, n_class, n_confidence_level)
+        Shapes: (n, ) and (n, n_class, n_confidence_levels)
         """
-        return np.ndarray(0), np.ndarray(0)
+        conformity_score_params_ = prepare_params(conformity_score_params)
+        predictions = self._mapie_classifier.predict(
+            X,
+            alpha=self._alphas,
+            include_last_label=conformity_score_params_.get("include_last_label", True),
+            **self._predict_params,
+        )
+        return cast_predictions_to_ndarray_tuple(predictions)
+
+    def predict(self, X: ArrayLike) -> NDArray:
+        predictions = self._mapie_classifier.predict(
+            X,
+            alpha=None,
+            **self._predict_params,
+        )
+        return cast_point_predictions_to_ndarray(predictions)
 
 
 class CrossConformalClassifier:
