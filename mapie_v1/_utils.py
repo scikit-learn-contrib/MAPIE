7435633d1ffd547b13869c40e0f4aefd68c36a2a
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-03-11 17:02:13 +0100

    ENH & TEST & MTN: implement guardrails to avoid method being called in incorrect order. Add related functional tests. Unrelated: simplify existing functional test, add and use transform_confidence_level_to_alpha in ConformalizedQuantileRegressor (#622)

--- mapie_v1/_utils.py
+++ mapie_v1/_utils.py
@@ -14,14 +14,21 @@ def transform_confidence_level_to_alpha_list(
         confidence_levels = confidence_level
     else:
         confidence_levels = [confidence_level]
+    return [
+        transform_confidence_level_to_alpha(confidence_level)
+        for confidence_level in confidence_levels
+    ]
 
+
+def transform_confidence_level_to_alpha(
+    confidence_level: float,
+) -> float:
     # Using decimals to avoid weird-looking float approximations
     # when computing alpha = 1 - confidence_level
     # Such approximations arise even with simple confidence levels like 0.9
-    confidence_levels_d = [Decimal(str(conf_level)) for conf_level in confidence_levels]
-    alphas_d = [Decimal("1") - conf_level_d for conf_level_d in confidence_levels_d]
-
-    return [float(alpha_d) for alpha_d in alphas_d]
+    confidence_level_decimal = Decimal(str(confidence_level))
+    alpha_decimal = Decimal("1") - confidence_level_decimal
+    return float(alpha_decimal)
 
 
 def check_if_param_in_allowed_values(
@@ -63,3 +70,36 @@ def prepare_fit_params_and_sample_weight(
     fit_params_ = prepare_params(fit_params)
     sample_weight = fit_params_.pop("sample_weight", None)
     return fit_params_, sample_weight
+
+
+def raise_error_if_previous_method_not_called(
+    current_method_name: str,
+    previous_method_name: str,
+    was_previous_method_called: bool,
+) -> None:
+    if not was_previous_method_called:
+        raise ValueError(
+            f"Incorrect method order: call {previous_method_name} "
+            f"before calling {current_method_name}."
+        )
+
+
+def raise_error_if_method_already_called(
+    method_name: str,
+    was_method_called: bool,
+) -> None:
+    if was_method_called:
+        raise ValueError(
+            f"{method_name} method already called. "
+            f"MAPIE does not currently support calling {method_name} several times."
+        )
+
+
+def raise_error_if_fit_called_in_prefit_mode(
+    is_mode_prefit: bool,
+) -> None:
+    if is_mode_prefit:
+        raise ValueError(
+            "The fit method must be skipped when the prefit parameter is set to True. "
+            "Use the conformalize method directly after instanciation."
+        )
