bb5711603b3c63a83ccf33c879152d8d7149cf42
Cyprien Bertran de Balanda <cyprien.bertran-de-balanda@capgemini.com>
2025-03-13 16:55:43 +0100

    Migrate plot_cqr_symmetry_difference.py

--- examples/regression/1-quickstart/plot_cqr_symmetry_difference.py
+++ examples/regression/1-quickstart/plot_cqr_symmetry_difference.py
@@ -1,55 +1,70 @@
 """
 ===================================================================
-[Pre-v1] Plotting CQR with symmetric argument
+Plotting CQR with symmetric argument
 ===================================================================
 **Note: we recently released MAPIE v1.0.0, which introduces breaking API changes.**
-**This notebook hasn't been updated to the new API yet.**
 
-An example plot of :class:`~mapie.quantile_regression.MapieQuantileRegressor`
+An example plot of :class:`~mapie_v1.regression.ConformalizedQuantileRegressor`
 illustrating the impact of the symmetry parameter.
 """
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.datasets import make_regression
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
 
 from mapie.metrics import regression_coverage_score
-from mapie.quantile_regression import MapieQuantileRegressor
+from mapie_v1.regression import ConformalizedQuantileRegressor
 
-random_state = 2
+RANDOM_STATE = 1
 
 ##############################################################################
 # We generate a synthetic data.
 
-X, y = make_regression(n_samples=500, n_features=1, noise=20, random_state=59)
+X, y = make_regression(
+    n_samples=1000, n_features=1, noise=20, random_state=RANDOM_STATE
+)
+
+X_train_conformalize, X_test, y_train_conformalize, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=RANDOM_STATE
+)
+
+X_train, X_conformalize, y_train, y_conformalize = train_test_split(
+    X_train_conformalize, y_train_conformalize, test_size=0.3, random_state=RANDOM_STATE
+)
 
 # Define alpha level
-alpha = 0.2
+confidence_level = 0.8
 
 # Fit a Gradient Boosting Regressor for quantile regression
 gb_reg = GradientBoostingRegressor(
-    loss="quantile", alpha=0.5, random_state=random_state
+    loss="quantile", alpha=0.5, random_state=RANDOM_STATE
 )
 
 # MAPIE Quantile Regressor
-mapie_qr = MapieQuantileRegressor(estimator=gb_reg, alpha=alpha)
-mapie_qr.fit(X, y, random_state=random_state)
-y_pred_sym, y_pis_sym = mapie_qr.predict(X, symmetry=True)
-y_pred_asym, y_pis_asym = mapie_qr.predict(X, symmetry=False)
-y_qlow = mapie_qr.estimators_[0].predict(X)
-y_qup = mapie_qr.estimators_[1].predict(X)
+mapie_qr = ConformalizedQuantileRegressor(
+    estimator=gb_reg, confidence_level=confidence_level)
+mapie_qr.fit(X_train, y_train)
+mapie_qr.conformalize(X_conformalize, y_conformalize)
+y_pred_sym, y_pis_sym = mapie_qr.predict_interval(X_test, symmetric_correction=True)
+y_pred_asym, y_pis_asym = mapie_qr.predict_interval(X_test, symmetric_correction=False)
+y_qlow = mapie_qr._mapie_quantile_regressor.estimators_[0].predict(X_test)
+y_qup = mapie_qr._mapie_quantile_regressor.estimators_[1].predict(X_test)
 
+print(f"y.shape: {y.shape}")
+print(f"y_pis_sym[:, 0].shape: {y_pis_sym[:, 0].shape}")
+print(f"y_pis_sym[:, 1].shape: {y_pis_sym[:, 1].shape}")
 # Calculate coverage scores
 coverage_score_sym = regression_coverage_score(
-    y, y_pis_sym[:, 0], y_pis_sym[:, 1]
+    y_test, y_pis_sym[:, 0], y_pis_sym[:, 1]
 )
 coverage_score_asym = regression_coverage_score(
-    y, y_pis_asym[:, 0], y_pis_asym[:, 1]
+    y_test, y_pis_asym[:, 0], y_pis_asym[:, 1]
 )
 
 # Sort the values for plotting
-order = np.argsort(X[:, 0])
-X_sorted = X[order]
+order = np.argsort(X_test[:, 0])
+X_test_sorted = X_test[order]
 y_pred_sym_sorted = y_pred_sym[order]
 y_pis_sym_sorted = y_pis_sym[order]
 y_pred_asym_sorted = y_pred_asym[order]
@@ -68,13 +83,13 @@ plt.figure(figsize=(14, 7))
 plt.subplot(1, 2, 1)
 plt.xlabel("x")
 plt.ylabel("y")
-plt.scatter(X, y, alpha=0.3)
-plt.plot(X_sorted, y_qlow, color="C1")
-plt.plot(X_sorted, y_qup, color="C1")
-plt.plot(X_sorted, y_pis_sym_sorted[:, 0], color="C1", ls="--")
-plt.plot(X_sorted, y_pis_sym_sorted[:, 1], color="C1", ls="--")
+plt.scatter(X_test, y_test, alpha=0.3)
+plt.plot(X_test_sorted, y_qlow, color="C1")
+plt.plot(X_test_sorted, y_qup, color="C1")
+plt.plot(X_test_sorted, y_pis_sym_sorted[:, 0], color="C1", ls="--")
+plt.plot(X_test_sorted, y_pis_sym_sorted[:, 1], color="C1", ls="--")
 plt.fill_between(
-    X_sorted.ravel(),
+    X_test_sorted.ravel(),
     y_pis_sym_sorted[:, 0].ravel(),
     y_pis_sym_sorted[:, 1].ravel(),
     alpha=0.2,
@@ -82,20 +97,20 @@ plt.fill_between(
 plt.title(
     f"Symmetric Intervals\n"
     f"Target and effective coverages for "
-    f"alpha={alpha:.2f}: ({1-alpha:.3f}, {coverage_score_sym:.3f})"
+    f"confidence_level={confidence_level:.2f}; coverage={coverage_score_sym:.3f})"
 )
 
 # Plot asymmetric prediction intervals
 plt.subplot(1, 2, 2)
 plt.xlabel("x")
 plt.ylabel("y")
-plt.scatter(X, y, alpha=0.3)
-plt.plot(X_sorted, y_qlow, color="C2")
-plt.plot(X_sorted, y_qup, color="C2")
-plt.plot(X_sorted, y_pis_asym_sorted[:, 0], color="C2", ls="--")
-plt.plot(X_sorted, y_pis_asym_sorted[:, 1], color="C2", ls="--")
+plt.scatter(X_test, y_test, alpha=0.3)
+plt.plot(X_test_sorted, y_qlow, color="C2")
+plt.plot(X_test_sorted, y_qup, color="C2")
+plt.plot(X_test_sorted, y_pis_asym_sorted[:, 0], color="C2", ls="--")
+plt.plot(X_test_sorted, y_pis_asym_sorted[:, 1], color="C2", ls="--")
 plt.fill_between(
-    X_sorted.ravel(),
+    X_test_sorted.ravel(),
     y_pis_asym_sorted[:, 0].ravel(),
     y_pis_asym_sorted[:, 1].ravel(),
     alpha=0.2,
@@ -103,7 +118,7 @@ plt.fill_between(
 plt.title(
     f"Asymmetric Intervals\n"
     f"Target and effective coverages for "
-    f"alpha={alpha:.2f}: ({1-alpha:.3f}, {coverage_score_asym:.3f})"
+    f"confidence_level={confidence_level:.2f}; coverage={coverage_score_sym:.3f})"
 )
 plt.tight_layout()
 plt.show()
@@ -114,4 +129,4 @@ plt.show()
 # each bound, allowing for more flexible and accurate intervals that reflect
 # the heteroscedastic nature of the data. The resulting effective coverages
 # demonstrate the theoretical guarantee of the target coverage level
-# ``1 - α``.
+# ``confidence_level``.
