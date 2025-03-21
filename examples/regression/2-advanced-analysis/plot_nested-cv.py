371fba3f794baed25e6ead80680657ff9e1fc08f
C-BdB <137887330+C-BdB@users.noreply.github.com>
2025-03-21 15:00:07 +0100

    DOC: Migrate every regression examples and notebooks to v1. (#624)
    * Migrate homoscedastic, heteroscedastic and gamma examples
    
    * Migrate plot_coverage_validity.py
    
    * Migrate plot_barber2020_simulations.py
    
    * Migrate plot_kim2020_simulations.py
    
    * Migrate plot_conformal_predictive_distribution.py
    
    * Migrate plot_cqr_symmetry_difference.py
    
    * Migrate plot_nested-cv.py to v1
    
    * Migrate plot_both_uncertainties.py to v1
    
    * Migrate plot-coverage-width-based-criterion.py to v1
    
    * Migrate plot_conditional_coverage.py to v1
    
    * Migrate plot_ResidualNormalisedScore_tutorial.py to v1
    
    * Migrate plot_cqr_tutorial.py to v1
    
    * Migrate two exoplanets.ipynb and tutorial_regression.ipynb to v1
    
    * Few edits from code review
    
    * Delete mentions to 'pre-v1'
    
    * Fix lint
    
    * Migrate plot_main-tutorial-regression.py to v1
    
    * Change titles and arborescence of regression examples
    
    * Delete optimize_beta parameter from examples as it does not work.
    
    * Restore subtitles on plot_prefit. The gestion of the subtitles is on another ticket.
    
    * Make lint
    
    * Fix typo and links
    
    * Fix bullet point lists.
    
    * Delete v1 mention on the regression examples README.rst
    
    ---------
    
    Co-authored-by: Valentin Laurent <valentin.laurent.fr@gmail.com>

--- examples/regression/2-advanced-analysis/plot_nested-cv.py
+++ examples/regression/2-advanced-analysis/plot_nested-cv.py
@@ -1,42 +1,33 @@
 """
 ==========================================================================================
-[Pre-v1] Nested cross-validation for estimating prediction intervals
+Hyperparameters tuning with CrossConformalRegressor
 ==========================================================================================
-**Note: we recently released MAPIE v1.0.0, which introduces breaking API changes.**
-**This notebook hasn't been updated to the new API yet.**
 
 
-This example compares non-nested and nested cross-validation strategies for
-estimating prediction intervals with :class:`~mapie.regression.MapieRegressor`.
+This example compares non-nested and nested cross-validation strategies
+when using
+:class:`~mapie_v1.regression.CrossConformalRegressor`.
 
-In the regular sequential method, a cross-validation parameter search is
-carried out over the entire training set.
-The model with the set of parameters that gives the best score is then used in
-MAPIE to estimate the prediction intervals associated with the predictions.
-A limitation of this method is that residuals used by MAPIE are computed on
-the validation dataset, which can be subject to overfitting as far as
-hyperparameter tuning is concerned.
+In the regular sequential method, a cross-validation parameter search is performed
+on the entire training set. The best model is then used in MAPIE to estimate
+prediction intervals. However, as MAPIE computes residuals on
+the validation dataset used during hyperparameter tuning, it can lead to
+overfitting. This fools MAPIE into being slightly too optimistic with confidence
+intervals.
 
-This fools MAPIE into being slightly too optimistic with confidence intervals.
 To solve this problem, an alternative option is to perform a nested
 cross-validation parameter search directly within the MAPIE estimator on each
 *out-of-fold* dataset.
-For each testing fold used by MAPIE to store residuals, an internal
-cross-validation occurs on the training fold, optimizing hyperparameters.
 This ensures that residuals seen by MAPIE are never seen by the algorithm
 beforehand. However, this method is much heavier computationally since
 it results in ``N * P`` calculations, where *N* is the number of
 *out-of-fold* models and *P* the number of parameter search cross-validations,
 versus ``N + P`` for the non-nested approach.
 
-Here, we compare the two strategies on a toy dataset. We use the Random
-Forest Regressor as a base regressor for the CV+ strategy. For the sake of
-light computation, we adopt a RandomizedSearchCV parameter search strategy
-with a low number of iterations and with a reproducible random state.
+Here, we compare the two strategies on a toy dataset.
 
 The two approaches give slightly different predictions with the nested CV
-approach estimating slightly larger prediction interval widths by a
-few percents at most (apart from a handful of exceptions).
+approach estimating larger prediction interval in average.
 
 For this example, the two approaches result in identical scores and identical
 effective coverages.
@@ -50,32 +41,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import randint
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from sklearn.datasets import make_sparse_uncorrelated
 
 from mapie.metrics import regression_coverage_score
-from mapie.regression import MapieRegressor
+from mapie_v1.regression import CrossConformalRegressor
 
 
-random_state = 42
+RANDOM_STATE = 42
 
 # Load the toy data
-X, y = make_sparse_uncorrelated(500, random_state=random_state)
+X, y = make_sparse_uncorrelated(500, random_state=RANDOM_STATE)
 
 # Split the data into training and test sets.
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=random_state
+    X, y, test_size=0.2, random_state=RANDOM_STATE
 )
 
 # Define the Random Forest model as base regressor with parameter ranges.
-rf_model = RandomForestRegressor(random_state=random_state, verbose=0)
+rf_model = RandomForestRegressor(random_state=RANDOM_STATE, verbose=0)
 rf_params = {"max_depth": randint(2, 10), "n_estimators": randint(10, 100)}
 
 # Cross-validation and prediction-interval parameters.
 cv = 10
 n_iter = 5
-alpha = 0.05
+confidence_level = 0.95
 
 # Non-nested approach with the CV+ strategy using the Random Forest model.
 cv_obj = RandomizedSearchCV(
@@ -86,24 +77,24 @@ cv_obj = RandomizedSearchCV(
     scoring="neg_root_mean_squared_error",
     return_train_score=True,
     verbose=0,
-    random_state=random_state,
+    random_state=RANDOM_STATE,
     n_jobs=-1,
 )
 cv_obj.fit(X_train, y_train)
 best_est = cv_obj.best_estimator_
-mapie_non_nested = MapieRegressor(
-    best_est, method="plus", cv=cv, agg_function="median", n_jobs=-1,
-    random_state=random_state
+mapie_non_nested = CrossConformalRegressor(
+    estimator=best_est, method="plus", cv=cv, n_jobs=-1,
+    confidence_level=confidence_level, random_state=RANDOM_STATE
 )
-mapie_non_nested.fit(X_train, y_train)
-y_pred_non_nested, y_pis_non_nested = mapie_non_nested.predict(
-    X_test, alpha=alpha
+mapie_non_nested.fit_conformalize(X_train, y_train)
+y_pred_non_nested, y_pis_non_nested = mapie_non_nested.predict_interval(
+    X_test, aggregate_predictions='median'
 )
 widths_non_nested = y_pis_non_nested[:, 1, 0] - y_pis_non_nested[:, 0, 0]
 coverage_non_nested = regression_coverage_score(
     y_test, y_pis_non_nested[:, 0, 0], y_pis_non_nested[:, 1, 0]
 )
-score_non_nested = mean_squared_error(y_test, y_pred_non_nested, squared=False)
+score_non_nested = root_mean_squared_error(y_test, y_pred_non_nested)
 
 # Nested approach with the CV+ strategy using the Random Forest model.
 cv_obj = RandomizedSearchCV(
@@ -114,20 +105,22 @@ cv_obj = RandomizedSearchCV(
     scoring="neg_root_mean_squared_error",
     return_train_score=True,
     verbose=0,
-    random_state=random_state,
+    random_state=RANDOM_STATE,
     n_jobs=-1,
 )
-mapie_nested = MapieRegressor(
-    cv_obj, method="plus", cv=cv, agg_function="median",
-    random_state=random_state
+mapie_nested = CrossConformalRegressor(
+    estimator=cv_obj, method="plus", cv=cv, n_jobs=-1,
+    confidence_level=confidence_level, random_state=RANDOM_STATE
 )
-mapie_nested.fit(X_train, y_train)
-y_pred_nested, y_pis_nested = mapie_nested.predict(X_test, alpha=alpha)
+mapie_nested.fit_conformalize(X_train, y_train)
+y_pred_nested, y_pis_nested = mapie_nested.predict_interval(
+    X_test, aggregate_predictions='median'
+)
 widths_nested = y_pis_nested[:, 1, 0] - y_pis_nested[:, 0, 0]
 coverage_nested = regression_coverage_score(
     y_test, y_pis_nested[:, 0, 0], y_pis_nested[:, 1, 0]
 )
-score_nested = mean_squared_error(y_test, y_pred_nested, squared=False)
+score_nested = root_mean_squared_error(y_test, y_pred_nested)
 
 # Print scores and effective coverages.
 print(
--- examples/regression/README.rst
+++ examples/regression/README.rst
@@ -1,8 +1,6 @@
 .. _regression_examples:
 
-Regression examples
-===================
+All regression examples
+========================
 
 Following is a collection of notebooks demonstrating how to use MAPIE.
-
-**Note: we recently released MAPIE v1.0.0, which introduces breaking API changes. Not all notebooks have been updated to the new API yet. These are marked as `[Pre-v1]`.**
