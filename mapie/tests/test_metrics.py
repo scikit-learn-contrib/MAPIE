82854fe70d25b5f157753d417603321e794a9735
FaustinPulveric <faustin.pulveric@capgemini.com>
2025-04-17 16:45:53 +0200

    FIX: improve tests for classification_mean_width_score and regression_mean_width_score

--- mapie/tests/test_metrics.py
+++ mapie/tests/test_metrics.py
@@ -318,15 +318,14 @@ def test_classification_y_pred_set_type() -> None:
     assert scr == 0.8
 
 
-@pytest.mark.parametrize("pred_set", [y_pred_set, list(y_pred_set)])
-def test_classification_toydata_width(pred_set: ArrayLike) -> None:
-    """Test width mean for toy data."""
+def test_classification_mean_width_score_toydata() -> None:
+    """Test classification_mean_width_score for toy data."""
     scr = classification_mean_width_score(y_pred_set_2alphas)
     np.testing.assert_allclose(scr, [2.2, 1.8], rtol=1e-2, atol=1e-2)
 
 
-def test_regression_toydata_mean_width_score() -> None:
-    """Test mean_width_score for toy data."""
+def test_regression_mean_width_score_toydata() -> None:
+    """Test regression_mean_width_score for toy data."""
     scr = regression_mean_width_score(intervals)
     np.testing.assert_allclose(scr, [2.3, 2.2], rtol=1e-2, atol=1e-2)
 
