e67b3dbe59bda3290adf7da885dca3dbfab290d4
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-05-06 13:58:07 +0200

    MTN: clarify public/private API in __init__ modules (#678)

--- mapie/__init__.py
+++ mapie/__init__.py
@@ -1,10 +1,21 @@
-from . import classification, metrics, regression, utils
+from . import (
+    classification,
+    metrics,
+    regression,
+    utils,
+    multi_label_classification,
+    calibration,
+    subsample,
+)
 from ._version import __version__
 
 __all__ = [
     "regression",
     "classification",
+    "multi_label_classification",
+    "calibration",
     "metrics",
     "utils",
+    "subsample",
     "__version__"
 ]
