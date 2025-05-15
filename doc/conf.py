1b3fab60903fccfb9d93e7b3624fce81c4aa29c5
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-05-15 16:19:10 +0200

    CHORE: revert to proper versioning before releasing v1.0.0rc1, fix bump2version config file

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "1.0.0-alpha1"
+version = "0.9.2"
 # The full version, including alpha/beta/rc tags.
 release = version
 
