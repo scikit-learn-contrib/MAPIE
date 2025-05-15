d67f35ce1a0dc2e8cdda8607a95d183a75459482
FaustinPulveric <faustin.pulveric@capgemini.com>
2025-05-15 18:27:34 +0200

    Revert "DOC: make banner only visible in latest and stable versions"
    This reverts commit 16d2a1fa1926d8e6d9a1056ddf9e4daabb102754.

--- doc/_static/js/informative-banner.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var path = window.location.pathname;
-
-    if (path.includes('/latest/') || path.includes('/stable/')) {
-        document.getElementById('version-banner').style.display = 'block';
-    }
-});
