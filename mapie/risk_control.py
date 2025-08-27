78017bee239e9bdd05d091c00e32ed00137c8110
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-08-28 01:45:03 +0200

    WIP - try to fix typing (can't reproduce locally)

--- mapie/risk_control.py
+++ mapie/risk_control.py
@@ -741,7 +741,7 @@ class BinaryClassificationRisk:
             for y_true_i, y_pred_i in zip(y_true, y_pred)
         ])
         effective_sample_size = len(y_true) - np.sum(~risk_conditions)
-        effective_sample_size cast(int, effective_sample_size)  # Needed for MyPy with Python 3.9
+        effective_sample_size = cast(int, effective_sample_size)  # Needed for MyPy with Python 3.9
         if effective_sample_size != 0:
             risk_sum: int = np.sum(risk_occurrences[risk_conditions])
             risk_value = risk_sum / effective_sample_size
