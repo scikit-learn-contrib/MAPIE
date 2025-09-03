886c106a592cfcb70a65d6624f12a773e7fdeb97
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-09-03 10:50:47 +0200

    FIX typo

--- mapie/control_risk/ltt.py
+++ mapie/control_risk/ltt.py
@@ -104,7 +104,7 @@ def find_lambda_control_star(
     Returns
     -------
     l_lambda_star: ArrayLike of shape (n_alpha, ).
-        The lambda that give the highest precision
+        The lambda that give the minimum precision
         for a given alpha.
 
     r_star: ArrayLike of shape (n_alpha, ).
