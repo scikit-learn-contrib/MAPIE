4b031f5306dc79211f2cecf73cfaaceff21f15ee
Valentin Laurent <valentin.laurent.fr@gmail.com>
2025-09-22 17:58:03 +0200

    DOC - Refine target API following release of binary risk control first iteration

--- mapie/binary_risk_control_target_api.py
+++ mapie/binary_risk_control_target_api.py
@@ -1,62 +1,49 @@
+# pylint: disable=line-too-long
+
 from __future__ import annotations
-from typing import Callable, Optional, Tuple, Union, List, Any
+from typing import Callable, Optional, Tuple, Union, List, Any, Literal
 
 import numpy as np
 from numpy._typing import ArrayLike, NDArray
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
+from mapie.risk_control import precision, accuracy, BinaryClassificationRisk
 
-class BinaryClassificationRisk:
-    # Any risk that can be defined in the following way will work using the binary Hoeffding-Bentkus p-values used in MAPIE
-    # Take the example of precision in the docstring to explain how the class works.
+
+class BinaryClassificationRisk_:
     def __init__(
         self,
-        occurrence: Callable[[int, int], Optional[float]],
-        # (y_true, y_pred) (y_pred possibly None), output: float between 0 and 1 or None if undefined
+        risk_occurrence: Callable[[int, int], bool],
+        risk_condition: Callable[[int, int], bool],
+        # or Callable[[int, int], Union[bool, int]] if binary allowed
         higher_is_better: bool,
         binary: bool,  # True if occurrence is binary (too advanced?)
     ):
-        self.occurrence = occurrence
+        self._risk_occurrence = risk_occurrence
+        self._risk_condition = risk_condition
         self.higher_is_better = higher_is_better
         self.binary = binary
 
     def get_value_and_effective_sample_size(
         self,
-        y_true: ArrayLike,
-        y_pred: ArrayLike
-    ) -> Optional[Tuple[float, int]]:  # value : float between 0 and 1, effective_sample_size: int between 0 and len(y_true)
-        pass  # this function will most probably be the same for all risks
-        # if the risk is not defined, return None
-
-    def transform_to_opposite(self) -> BinaryClassificationRisk:
-        def opposite_occurrence(y_true, y_pred):
-            None if not self.occurrence(y_true, y_pred) else 1 - self.occurrence(
-                y_true,
-                y_pred
-            )
-
-        return BinaryClassificationRisk(
-            occurrence=opposite_occurrence,
-            higher_is_better=not self.higher_is_better,
-            binary=self.binary
-        )
+        y_true: NDArray,
+        y_pred: NDArray,
+    ) -> Tuple[float, int]:
+        return 0, 0
 
 
 class BinaryClassificationController:
     def __init__(
         self,
-        predict_function: Union[Callable[..., int], Callable[[Any], float]],
-        # either a predict_proba (X -> proba), either anything predicting using parameters ((X, *params) -> 0/1)
-        risk: Union[BinaryClassificationRisk | List[BinaryClassificationRisk]],
+        predict_function: Callable[..., NDArray],  # either a predict_proba (X -> proba), either anything predicting using parameters ((X, *params) -> 0/1)
+        risk: Union[BinaryClassificationRisk, List[BinaryClassificationRisk]],
         target_level: Union[float, List[float]],
         confidence_level: float = 0.9,
-        best_predict_param_choice: Union[str, BinaryClassificationRisk] = "auto",
-        # Can't be "auto" in multi-risk or for custom risk
-        predict_params: Optional[NDArray] = None,  # Can't be None if predict_function is not a predict_proba
-        # An array of parameters of shape (nb_combinations, nb_parameters), or shape (nb_combinations, ) in case of a unidimensional parameter, explicitly listing all possible joint values of the parameters
-        predict_params_graph=None,
-        # list of lists (for each param value, the list of its neighbors), we start with the first element of the list of params_values
+        best_predict_param_choice: Union[
+            Literal["auto"], BinaryClassificationRisk] = "auto",  # Shall we allow "auto" for multi-risk ?
+        predict_params: Optional[NDArray] = None,  # Can't be None if predict_function is not a predict_proba. An array of parameters of shape (nb_combinations, nb_parameters), or shape (nb_combinations, ) in case of a unidimensional parameter, explicitly listing all possible joint values of the parameters
+        predict_params_graph=None,  # list of lists (for each param value, the list of its neighbors), we start with the first element of the list of params_values
     ):
         self.predict_function = predict_function
         self.risk = risk
@@ -65,50 +52,22 @@ class BinaryClassificationController:
         self.best_predict_param_choice = best_predict_param_choice
         self.predict_params = predict_params
         self.predict_params_graph = predict_params_graph
+        return
 
     def calibrate(
         self,
         X_calibrate: ArrayLike,
         y_calibrate: ArrayLike,
     ) -> BinaryClassificationController:
-        pass
+        return self
 
     def predict(self, X_test: ArrayLike) -> NDArray:
-        pass
+        return np.array([])
 
 
 # Examples
 clf = LogisticRegression()  #.fit(X_train, y_train)
 
-precision = BinaryClassificationRisk(
-    occurrence=lambda y_true, y_pred: None if y_pred == 0 else int(y_pred == y_true),
-    higher_is_better=True,
-    binary=True,
-)
-
-false_discovery_rate = precision.transform_to_opposite()
-
-accuracy = BinaryClassificationRisk(
-    occurrence=lambda y_true, y_pred: int(y_pred == y_true),
-    higher_is_better=True,
-    binary=True,
-)
-
-# Simplest API
-BinaryClassificationController(
-    predict_function=clf.predict_proba,
-    risk=precision,
-    target_level=0.8,
-)
-
-# Simple API with custom choice
-BinaryClassificationController(
-    predict_function=clf.predict_proba,
-    risk=false_discovery_rate,
-    target_level=0.7,
-    confidence_level=0.95,
-    best_predict_param_choice=accuracy,
-)
 # Multi-risk
 BinaryClassificationController(
     predict_function=clf.predict_proba,
@@ -117,15 +76,13 @@ BinaryClassificationController(
     best_predict_param_choice=precision,
 )
 
-
 # Dummy definitions to demonstrate use case 3
 def abstention_classifier(X, param_1, param_2):
-    return 1
+    return np.array([1] * len(X))  # dummy
 
-
-negative_predictive_value = precision
-abstention_rate = precision
-predict_params = []
+negative_predictive_value = precision  # dummy
+abstention_rate = precision  # dummy
+predict_params = np.array([])  # dummy
 
 # Use case 3: abstention classifier with multiple risks and multi-dimensional lambda
 controller_use_case_3 = BinaryClassificationController(
@@ -140,10 +97,8 @@ controller_use_case_3 = BinaryClassificationController(
 # Using SVC decision_function
 svc = SVC()  #.fit()
 
-
 def svc_predict(X, threshold):
-    return svc.decision_function(X) >= threshold
-
+    return (np.array(svc.decision_function(X)) >= threshold).astype(int)
 
 controller_svc = BinaryClassificationController(
     predict_function=svc_predict,
