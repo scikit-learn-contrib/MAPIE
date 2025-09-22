4b21440ffbed11ae67644d3b2ce40b49b4017c4c
Adrien Le Coz <adrien.le-coz@inria.fr>
2025-09-22 15:44:30 +0200

    Bump version: 1.0.1 → 1.1.0

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "1.0.1"
+version = "1.1.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
