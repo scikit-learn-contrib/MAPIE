32ae623efd1af60a82b3d24fd2f964ba09a06f6b
Hassan Maissoro <maissorohassan@gmail.com>
2025-10-31 17:44:40 +0100

    handle exit(0)

--- mapie/tests/notebooks/_run_notebooks.py
+++ mapie/tests/notebooks/_run_notebooks.py
@@ -7,22 +7,23 @@ from nbformat import read
 NOTEBOOKS_DIR = Path(__file__).parent
 notebooks = NOTEBOOKS_DIR.rglob("*.ipynb")
 
-success = True
+if __name__ == "__main__":
+    success = True
 
-for nb_path in notebooks:
-    print(f"Running {nb_path} ...")
-    try:
-        with nb_path.open() as f:
-            nb = read(f, as_version=4)
-        client = NotebookClient(nb, timeout=60)
-        client.execute()
-    except Exception as e:
-        print(f"Notebook {nb_path} failed:\n{e}")
-        success = False
+    for nb_path in notebooks:
+        print(f"Running {nb_path} ...")
+        try:
+            with nb_path.open() as f:
+                nb = read(f, as_version=4)
+            client = NotebookClient(nb, timeout=60)
+            client.execute()
+        except Exception as e:
+            print(f"Notebook {nb_path} failed:\n{e}")
+            success = False
 
-if success:
-    print("\nAll notebooks executed successfully. 100% OK.\n")
-    sys.exit(0)
-else:
-    print("\nSome notebooks failed.")
-    sys.exit(1)
+    if success:
+        print("\nAll notebooks executed successfully. 100% OK.\n")
+        sys.exit(0)
+    else:
+        print("\nSome notebooks failed.")
+        sys.exit(1)
