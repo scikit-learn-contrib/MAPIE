85da595d9d7c470fb3307d368150dbb8f6e302de
Hassan Maissoro <maissorohassan@gmail.com>
2025-11-03 17:30:54 +0100

    stop at first error

--- mapie/tests/notebooks/_run_notebooks.py
+++ mapie/tests/notebooks/_run_notebooks.py
@@ -9,8 +9,6 @@ NOTEBOOKS_DIR = Path(__file__).parent
 notebooks = NOTEBOOKS_DIR.rglob("*.ipynb")
 
 if __name__ == "__main__":  # pragma: no cover
-    success = True
-
     for nb_path in notebooks:
         print(f"Running {nb_path} ...")
         try:
@@ -20,11 +18,7 @@ if __name__ == "__main__":  # pragma: no cover
             client.execute()
         except Exception as e:
             print(f"Notebook {nb_path} failed:\n{e}")
-            success = False
+            sys.exit(1)
 
-    if success:
-        print("\nAll notebooks executed successfully. 100% OK.\n")
-        sys.exit(0)
-    else:
-        print("\nSome notebooks failed.")
-        sys.exit(1)
+    print("\nAll notebooks executed successfully. 100% OK.\n")
+    sys.exit(0)
