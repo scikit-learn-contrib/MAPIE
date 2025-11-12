FIX: Passed Predict Params to RAPS for conformity score calculation and EnsembleClassifier (#784)
* Passed Predict Params to RAPS for conformity score calculation and EnsembleClassifier

* Updated Authors.rst rst

* fix format issues

* Updated test case for RAPS and made  conformity score calculation code simpler in raps

mapie/tests/test_classification.py
@@ -1717,7 +1717,8 @@ def test_raise_error_new_class() -> None:
     """
     clf = LogisticRegression()
     clf.fit(X, y)
-    y[-1] = 10
+    y_modified = y.copy()
+    y_modified[-1] = 10
     mapie_clf = _MapieClassifier(
         estimator=clf,
         conformity_score=APSConformityScore(),
@@ -1725,7 +1726,7 @@ def test_raise_error_new_class() -> None:
         random_state=random_state,
     )
     with pytest.raises(ValueError, match=r".*Values in y do not matched values.*"):
-        mapie_clf.fit(X, y)
+        mapie_clf.fit(X, y_modified)
 
 
 def test_fit_parameters_passing() -> None:
@@ -1762,6 +1763,28 @@ def test_predict_parameters_passing() -> None:
 
     y_pred = mapie_model.predict(X_test, agg_scores="mean", **predict_params)
     np.testing.assert_equal(y_pred, 0)
+
+
+def test_raps_with_predict_params_passing() -> None:
+    """Test that predict_params are correctly passed when using RAPS."""
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=random_state
+    )
+    custom_gbc = CustomGradientBoostingClassifier(random_state=random_state)
+    raps_score = RAPSConformityScore(size_raps=0.1)
+    mapie_model = _MapieClassifier(
+        estimator=custom_gbc,
+        conformity_score=raps_score,
+        cv="split",
+        test_size=0.2,
+        random_state=42,
+    )
+    predict_params = {"check_predict_params": True}
+    mapie_model = mapie_model.fit(X_train, y_train, predict_params=predict_params)
+    # Since check_predict_params=True, y_pred_proba_raps should have all probabilities set to 1.0 for class 0
+    assert np.allclose(raps_score.y_pred_proba_raps[:, 0], 1.0), (
+        "predict_params are not passed successfully to raps score"
+    )
 
 
 def test_with_no_predict_parameters_passing() -> None: