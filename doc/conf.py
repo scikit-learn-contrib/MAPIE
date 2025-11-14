665c2679cfeff3cefe3332695305fa5bc8fe0f6a
GBrelurut <geoffraybrelurut@wanadoo.fr>
2025-11-14 18:19:29 +0100

    Bump version: 1.1.0 → 1.2.0

--- doc/conf.py
+++ doc/conf.py
@@ -88,7 +88,7 @@ copyright = u"2022, Quantmetry"
 # built documents.
 #
 # The short X.Y version.
-version = "1.1.0"
+version = "1.2.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
