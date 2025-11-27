1aea07b81e0abbba7ddfd057d05d85238cbe33a9
Hassan Maissoro <maissorohassan@gmail.com>
2025-11-27 10:59:26 +0100

    add proportion_positive binary classif risk

--- mapie/risk_control/risks.py
+++ mapie/risk_control/risks.py
@@ -271,3 +271,9 @@ false_positive_rate = BinaryClassificationRisk(
     risk_condition=lambda y_true, y_pred: y_true == 0,
     higher_is_better=False,
 )
+
+proportion_positive = BinaryClassificationRisk(
+    risk_occurrence=lambda y_true, y_pred: y_pred,
+    risk_condition=lambda y_true, y_pred: True,
+    higher_is_better=False,
+)
