ff6cc24486805d213c1de4da4809187030feab20
vincentblot28 <vincentblot28@gmail.com>
2025-12-17 11:46:08 +0100

    ADD: precision and recall for images

--- mapie/risk_control/risks.py
+++ mapie/risk_control/risks.py
@@ -4,144 +4,6 @@ from typing import Callable, Tuple, cast
 
 import numpy as np
 from numpy.typing import NDArray
-from sklearn.utils.validation import column_or_1d
-
-
-def compute_risk_recall_image(
-    lambdas: NDArray, y_pred_proba: NDArray, y: NDArray
-) -> NDArray:
-    """
-    In `SemanticSegmentationController` when `metric_control=recall`,
-    compute the recall per observation for each different
-    thresholds lambdas.
-
-    Parameters
-    ----------
-    y_pred_proba: NDArray of shape (n_samples, height, width, 1)
-        Predicted probabilities for each pixel and each observation.
-
-    y: NDArray of shape (n_samples, height, width)
-        True labels.
-
-    lambdas: NDArray of shape (n_lambdas, )
-        Threshold that permit to compute recall.
-
-    Returns
-    -------
-    NDArray of shape (n_samples, n_lambdas)
-        Risks for each observation and each value of lambda.
-    """
-    if y_pred_proba.ndim != 4:
-        raise ValueError(
-            "y_pred_proba should be a 4d array, got an array of shape "
-            "{} instead.".format(y_pred_proba.shape)
-        )
-    if y.ndim != 3:
-        raise ValueError(
-            "y should be a 3d array, got an array of shape {} instead.".format(
-                y_pred_proba.shape
-            )
-        )
-    if not np.array_equal(y_pred_proba.shape, y[:, np.newaxis, ...].shape):
-        print(y_pred_proba.shape, y.shape)
-        raise ValueError("y and y_pred_proba could not be broadcast.")
-    lambdas = cast(NDArray, column_or_1d(lambdas))
-
-    # transform y_pred_proba and y to numpy if they are not already
-    if not isinstance(y_pred_proba, np.ndarray):
-        y_pred_proba = np.array(y_pred_proba)
-    if not isinstance(y, np.ndarray):
-        y = np.array(y)
-    n_lambdas = len(lambdas)
-    y_pred_proba_repeat = np.repeat(y_pred_proba, n_lambdas, axis=1)
-    lambdas = lambdas[np.newaxis, :, np.newaxis, np.newaxis]
-    y_pred_th = (y_pred_proba_repeat > lambdas).astype(int)
-    # add new axis to y after the first one
-    y_repeat = np.repeat(y[:, np.newaxis, ...], n_lambdas, axis=1)
-    
-    risks = 1 - (_true_positive_images(y_pred_th, y_repeat) / y.sum(axis=tuple(range(1, y.ndim)))[:, np.newaxis])
-    return risks    
-
-
-def compute_risk_precision_image(
-    lambdas: NDArray, y_pred_proba: NDArray, y: NDArray
-) -> NDArray:
-    """
-    In `SemanticSegmentationController` when `metric_control=precision`,
-    compute the precision per observation for each different
-    thresholds lambdas.
-
-    Parameters
-    ----------
-    y_pred_proba: NDArray of shape (n_samples, height, width, 1)
-        Predicted probabilities for each pixel and each observation.
-
-    y: NDArray of shape (n_samples, height, width)
-        True labels.
-
-    lambdas: NDArray of shape (n_lambdas, )
-        Threshold that permit to compute precision score.
-
-    Returns
-    -------
-    NDArray of shape (n_samples, n_lambdas)
-        Risks for each observation and each value of lambda.
-    """
-    if y_pred_proba.ndim != 4:
-        raise ValueError(
-            "y_pred_proba should be a 4d array, got an array of shape "
-            "{} instead.".format(y_pred_proba.shape)
-        )
-    if y.ndim != 3:
-        raise ValueError(
-            "y should be a 3d array, got an array of shape {} instead.".format(
-                y_pred_proba.shape
-            )
-        )
-    if not np.array_equal(y_pred_proba.shape, y[:, np.newaxis, ...].shape):
-        print(y_pred_proba.shape, y.shape)
-        raise ValueError("y and y_pred_proba could not be broadcast.")
-    lambdas = cast(NDArray, column_or_1d(lambdas))
-
-    # transform y_pred_proba and y to numpy if they are not already
-    if not isinstance(y_pred_proba, np.ndarray):
-        y_pred_proba = np.array(y_pred_proba)
-    if not isinstance(y, np.ndarray):
-        y = np.array(y)
-    n_lambdas = len(lambdas)
-    y_pred_proba_repeat = np.repeat(y_pred_proba, n_lambdas, axis=1)
-    lambdas = lambdas[np.newaxis, :, np.newaxis, np.newaxis]
-    y_pred_th = (y_pred_proba_repeat > lambdas).astype(int)
-    # add new axis to y after the first one
-    y_repeat = np.repeat(y[:, np.newaxis, ...], n_lambdas, axis=1)
-
-    with np.errstate(divide="ignore", invalid="ignore"):
-        risks = 1 - _true_positive_images(y_pred_th, y_repeat) / y_pred_th.sum(
-            axis=tuple(range(2, y_pred_th.ndim))
-        )
-    risks[np.isnan(risks)] = 1  # nan value indicate high risks.    
-    return risks
-
-
-def _true_positive_images(y_pred_th: NDArray, y_repeat: NDArray) -> NDArray:
-    """
-    Compute the number of true positive.
-
-    Parameters
-    ----------
-    y_pred_proba : NDArray of shape (n_samples, n_labels, 1)
-        Predicted probabilities for each label and each observation.
-
-    y: NDArray of shape (n_samples, n_labels)
-        True labels.
-
-    Returns
-    -------
-    tp: float
-        The number of true positive.
-    """
-    tp = (y_pred_th * y_repeat).sum(axis=(-2, -1))
-    return tp
 
 
 class BinaryClassificationRisk:
@@ -269,6 +131,12 @@ precision = BinaryClassificationRisk(
     higher_is_better=True,
 )
 
+precision_image = BinaryClassificationRisk(
+    risk_occurrence=lambda y_true, y_pred: y_pred.ravel() == y_true.ravel(),
+    risk_condition=lambda y_true, y_pred: y_pred.ravel() == 1,
+    higher_is_better=True,
+)
+
 accuracy = BinaryClassificationRisk(
     risk_occurrence=lambda y_true, y_pred: y_pred == y_true,
     risk_condition=lambda y_true, y_pred: np.repeat(True, len(y_true)),
@@ -276,6 +144,12 @@ accuracy = BinaryClassificationRisk(
 )
 
 recall = BinaryClassificationRisk(
+    risk_occurrence=lambda y_true, y_pred: y_pred.ravel() == y_true.ravel(),
+    risk_condition=lambda y_true, y_pred: y_true.ravel() == 1,
+    higher_is_better=True,
+)
+
+recall_image = BinaryClassificationRisk(
     risk_occurrence=lambda y_true, y_pred: y_pred.ravel() == y_true.ravel(),
     risk_condition=lambda y_true, y_pred: y_true.ravel() == 1,
     higher_is_better=True,
