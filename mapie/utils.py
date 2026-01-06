6b5c180d0e2c326e1c9e513d4ddee3433ff33c96
Hassan Maissoro <maissorohassan@gmail.com>
2026-01-06 15:41:49 +0100

    chore: remove duplicated param in docstring

--- mapie/utils.py
+++ mapie/utils.py
@@ -407,17 +407,6 @@ def _check_no_agg_cv(
 
         By default ``None``.
 
-    y: Optional[ArrayLike] of shape (n_samples,)
-        Input labels.
-
-        By default ``None``.
-
-    groups: Optional[ArrayLike] of shape (n_samples,)
-        Group labels for the samples used while splitting the dataset into
-        train/test set.
-
-        By default ``None``.
-
     Returns
     -------
     bool
