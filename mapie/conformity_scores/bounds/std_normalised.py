9fcb9e8cbacad4e04d1cb2ed0ec6991bc09fb13e
EdgarJaber <edgar.jaber@icloud.com>
2026-01-12 15:27:44 +0100

    FIX: linting errors

--- mapie/conformity_scores/bounds/std_normalised.py
+++ mapie/conformity_scores/bounds/std_normalised.py
@@ -1,17 +1,10 @@
-import warnings
-from typing import Optional, Tuple, Union, cast
+from typing import Optional, Tuple, Union
 
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
-from sklearn.base import RegressorMixin, clone
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.utils import _safe_indexing
-from sklearn.utils.validation import check_random_state, indexable
+from sklearn.base import RegressorMixin
 
 from mapie.conformity_scores import BaseRegressionScore
-from mapie.utils import check_sklearn_user_model_is_fitted
 
 
 
