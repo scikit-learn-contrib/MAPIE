467df1ee835f102e917035b62fcf99ff920102c4
Hassan Maissoro <maissorohassan@gmail.com>
2026-02-03 15:19:16 +0100

    Bump version: 1.2.0 → 1.3.0

--- doc/conf.py
+++ doc/conf.py
@@ -87,7 +87,7 @@ copyright = "2026, Capgemini Invent"
 # built documents.
 #
 # The short X.Y version.
-version = "1.2.0"
+version = "1.3.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
