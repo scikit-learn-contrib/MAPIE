1fc5efdd8a1c90e14b1668f5fd31cec4ad217bf1
GBrelurut <geoffraybrelurut@wanadoo.fr>
2026-04-23 16:46:40 +0200

    Reorganize testing files

--- mapie/exchangeability_testing/__init__.py
+++ mapie/exchangeability_testing/__init__.py
@@ -4,10 +4,18 @@ from .exchangeability_testing import (
 )
 from .online_tests import OnlineMartingaleTest
 from .risk_monitoring import RiskMonitoring
+from .permutation_tests import (
+    PValuePermutationTest,
+    PermutationTest,
+    SequentialMonteCarloTest,
+)
 
 __all__ = [
     "FixedDatasetExchangeabilityTest",
     "OnlineExchangeabilityTest",
     "RiskMonitoring",
     "OnlineMartingaleTest",
+    "PValuePermutationTest",
+    "PermutationTest",
+    "SequentialMonteCarloTest",
 ]
