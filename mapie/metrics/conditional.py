7ae3f23c2bac7bdd1f5fb9c2233d420b7d484ebe
Adrien Le Coz <adrien.le-coz@inria.fr>
2026-06-29 14:53:00 +0200

    coverage_gap v0

--- mapie/metrics/conditional.py
+++ mapie/metrics/conditional.py
@@ -1,41 +1,30 @@
-from typing import Union
-
 import numpy as np
 from covmetrics import CovGap
 
-from mapie.classification import CrossConformalClassifier, SplitConformalClassifier
-from mapie.regression import (
-    ConformalizedQuantileRegressor,
-    CrossConformalRegressor,
-    JackknifeAfterBootstrapRegressor,
-    SplitConformalRegressor,
-    TimeSeriesRegressor,
-)
-
-MapieRegressors = Union[
-    SplitConformalRegressor,
-    CrossConformalRegressor,
-    JackknifeAfterBootstrapRegressor,
-    ConformalizedQuantileRegressor,
-    TimeSeriesRegressor,
-]
-
-MapieClassifiers = Union[
-    SplitConformalClassifier,
-    CrossConformalClassifier,
-]
+from mapie.utils import _transform_confidence_level_to_alpha
 
 
-def _compute_cover(mapie_estimator, x, y):
-    if isinstance(mapie_estimator, MapieRegressors):
-        _, y_intervals = mapie_estimator.predict_interval(x)
-        y_low = y_intervals[:, 0, :]
-        y_high = y_intervals[:, 1, :]
-        n_confidence_level = y_high.shape[1]
-        y_per_alpha = np.tile(y, (n_confidence_level, 1)).transpose()
-        return ((y_per_alpha >= y_low) & (y_per_alpha <= y_high)).astype(int)
+def _compute_cover(y, y_intervals=None, y_sets=None):
+    if y_intervals is not None:
+        if y_intervals.ndim == 3 and y_intervals.shape[-1] > 1:
+            raise ValueError
+        y_intervals = y_intervals.squeeze()
+        y_low = y_intervals[:, 0]
+        y_high = y_intervals[:, 1]
+        cover = (y >= y_low) & (y <= y_high)
+        return cover.astype(int)
+    if y_sets is not None:
+        if y_sets.ndim == 3 and y_sets.shape[-1] > 1:
+            raise ValueError
+        y_sets = y_sets.squeeze()
+        y = np.expand_dims(y, axis=1)
+        cover = np.take_along_axis(y_sets, y, axis=1).squeeze()
+        return cover.squeeze().astype(int)
 
 
-def coverage_gap(mapie_estimator, x, y, groups, alpha, weighted=False):
-    cover = compute_cover(mapie_estimator, x, y)
+def coverage_gap(
+    y, groups, confidence_level, y_intervals=None, y_sets=None, weighted=False
+):
+    cover = _compute_cover(y, y_intervals, y_sets)
+    alpha = _transform_confidence_level_to_alpha(confidence_level)
     return CovGap().evaluate(groups, cover, alpha, weighted)
