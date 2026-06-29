ad30565c1a4b3713202ab2736f678ac56e39f378
Adrien Le Coz <adrien.le-coz@inria.fr>
2026-06-29 12:01:27 +0200

    compute_cover regression

--- mapie/metrics/conditional.py
+++ mapie/metrics/conditional.py
@@ -1,15 +1,41 @@
+from typing import Union
+
+import numpy as np
 from covmetrics import CovGap
 
-MapieEstimator = Union[
-    SplitConformalClassifier,
+from mapie.classification import CrossConformalClassifier, SplitConformalClassifier
+from mapie.regression import (
+    ConformalizedQuantileRegressor,
+    CrossConformalRegressor,
+    JackknifeAfterBootstrapRegressor,
     SplitConformalRegressor,
+    TimeSeriesRegressor,
+)
+
+MapieRegressors = Union[
+    SplitConformalRegressor,
+    CrossConformalRegressor,
+    JackknifeAfterBootstrapRegressor,
+    ConformalizedQuantileRegressor,
+    TimeSeriesRegressor,
 ]
 
-def compute_cover(mapie_estimator, x, y):
-    if isinstance(mapie_estimator, SplitConformalRegressor):
-        _ (y_low, y_high) = mapie_estimator.predict_interval(x)
-    return (y >= y_low | y <= y_high)
+MapieClassifiers = Union[
+    SplitConformalClassifier,
+    CrossConformalClassifier,
+]
 
-def coverage_gap(mapie_estimator, y, groups, alpha, weighted=False):
-    cover = compute_cover(mapie_estimator, y)
+
+def _compute_cover(mapie_estimator, x, y):
+    if isinstance(mapie_estimator, MapieRegressors):
+        _, y_intervals = mapie_estimator.predict_interval(x)
+        y_low = y_intervals[:, 0, :]
+        y_high = y_intervals[:, 1, :]
+        n_confidence_level = y_high.shape[1]
+        y_per_alpha = np.tile(y, (n_confidence_level, 1)).transpose()
+        return ((y_per_alpha >= y_low) & (y_per_alpha <= y_high)).astype(int)
+
+
+def coverage_gap(mapie_estimator, x, y, groups, alpha, weighted=False):
+    cover = compute_cover(mapie_estimator, x, y)
     return CovGap().evaluate(groups, cover, alpha, weighted)
